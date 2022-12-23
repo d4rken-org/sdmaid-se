f52919a9b315ad8e23b5e4175210ff55a8b2992f
darken <darken@darken.eu>
2022-12-23 19:06:21 +0100

    Remove root check in settings for, not a smooth experience.

--- app/src/main/java/eu/darken/sdmse/corpsefinder/ui/settings/CorpseFinderSettingsFragment.kt
+++ app/src/main/java/eu/darken/sdmse/corpsefinder/ui/settings/CorpseFinderSettingsFragment.kt
@@ -7,7 +7,6 @@ import androidx.fragment.app.viewModels
 import androidx.preference.Preference
 import dagger.hilt.android.AndroidEntryPoint
 import eu.darken.sdmse.R
-import eu.darken.sdmse.common.observe2
 import eu.darken.sdmse.common.uix.PreferenceFragment2
 import eu.darken.sdmse.corpsefinder.core.CorpseFinderSettings
 import javax.inject.Inject
@@ -47,30 +46,30 @@ class CorpseFinderSettingsFragment : PreferenceFragment2() {
         findPreference<Preference>(settings.filterAppToSdEnabled.keyName)?.apply {
             summary = summary.toString() + "\n" + getString(R.string.general_root_required_message)
         }
-
-        vm.state.observe2(this) {
-            findPreference<Preference>(settings.filterPrivateDataEnabled.keyName)?.apply {
-                isEnabled = it.isRooted
-            }
-            findPreference<Preference>(settings.filterAppSourceAsecEnabled.keyName)?.apply {
-                isEnabled = it.isRooted
-            }
-            findPreference<Preference>(settings.filterDalvikCacheEnabled.keyName)?.apply {
-                isEnabled = it.isRooted
-            }
-            findPreference<Preference>(settings.filterAppLibEnabled.keyName)?.apply {
-                isEnabled = it.isRooted
-            }
-            findPreference<Preference>(settings.filterAppSourceEnabled.keyName)?.apply {
-                isEnabled = it.isRooted
-            }
-            findPreference<Preference>(settings.filterAppSourcePrivateEnabled.keyName)?.apply {
-                isEnabled = it.isRooted
-            }
-            findPreference<Preference>(settings.filterAppToSdEnabled.keyName)?.apply {
-                isEnabled = it.isRooted
-            }
-        }
+//
+//        vm.state.observe2(this) {
+//            findPreference<Preference>(settings.filterPrivateDataEnabled.keyName)?.apply {
+//                isEnabled = it.isRooted
+//            }
+//            findPreference<Preference>(settings.filterAppSourceAsecEnabled.keyName)?.apply {
+//                isEnabled = it.isRooted
+//            }
+//            findPreference<Preference>(settings.filterDalvikCacheEnabled.keyName)?.apply {
+//                isEnabled = it.isRooted
+//            }
+//            findPreference<Preference>(settings.filterAppLibEnabled.keyName)?.apply {
+//                isEnabled = it.isRooted
+//            }
+//            findPreference<Preference>(settings.filterAppSourceEnabled.keyName)?.apply {
+//                isEnabled = it.isRooted
+//            }
+//            findPreference<Preference>(settings.filterAppSourcePrivateEnabled.keyName)?.apply {
+//                isEnabled = it.isRooted
+//            }
+//            findPreference<Preference>(settings.filterAppToSdEnabled.keyName)?.apply {
+//                isEnabled = it.isRooted
+//            }
+//        }
     }
 
 }
