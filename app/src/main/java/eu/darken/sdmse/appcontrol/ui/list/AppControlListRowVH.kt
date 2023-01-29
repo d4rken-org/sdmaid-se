4b7ba22793a05bdac7a973cae76844e5b1e2f02c
darken <darken@darken.eu>
2023-01-29 21:04:51 +0100

    AppControl: Add state tags to apps

--- app/src/main/java/eu/darken/sdmse/appcontrol/ui/list/AppControlListRowVH.kt
+++ app/src/main/java/eu/darken/sdmse/appcontrol/ui/list/AppControlListRowVH.kt
@@ -1,10 +1,13 @@
 package eu.darken.sdmse.appcontrol.ui.list
 
 import android.view.ViewGroup
+import androidx.core.view.isInvisible
 import eu.darken.sdmse.R
 import eu.darken.sdmse.appcontrol.core.AppInfo
 import eu.darken.sdmse.common.coil.loadAppIcon
 import eu.darken.sdmse.common.lists.binding
+import eu.darken.sdmse.common.pkgs.isEnabled
+import eu.darken.sdmse.common.pkgs.isSystemApp
 import eu.darken.sdmse.databinding.AppcontrolListItemBinding
 
 
@@ -25,10 +28,10 @@ class AppControlListRowVH(parent: ViewGroup) :
         primary.text = appInfo.label.get(context)
         secondary.text = appInfo.pkg.packageName
 
-        primary.append(" (${appInfo.pkg.versionName ?: appInfo.pkg.versionCode})")
-//
-//        items.text = getQuantityString(R.plurals.result_x_items, junk.itemCount)
-//        size.text = Formatter.formatShortFileSize(context, junk.size)
+        tertiary.text = "${appInfo.pkg.versionName}  (${appInfo.pkg.versionCode})"
+
+        tagSystem.tagSystem.isInvisible = !appInfo.pkg.isSystemApp
+        tagDisabled.tagDisabled.isInvisible = appInfo.pkg.isEnabled
 //
         root.setOnClickListener { item.onItemClicked(appInfo) }
     }
