d84cc050086ad765a7d70c8e89652955936186b4
darken <darken@darken.eu>
2023-03-24 13:25:51 +0100

    Core: Better logging to identify which resources was closed.

--- app-common/src/main/java/eu/darken/sdmse/common/sharedresource/Resource.kt
+++ app-common/src/main/java/eu/darken/sdmse/common/sharedresource/Resource.kt
@@ -3,7 +3,7 @@ package eu.darken.sdmse.common.sharedresource
 data class Resource<T>(private val _item: T, val keepAlive: KeepAlive) : KeepAlive by keepAlive {
     val item: T
         get() {
-            check(!keepAlive.isClosed) { "Trying to access closed resource!" }
+            check(!keepAlive.isClosed) { "Trying to access closed resource! ($_item, $keepAlive)" }
             return _item
         }
 }
