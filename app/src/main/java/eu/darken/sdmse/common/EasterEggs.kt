939b9d6e8860e2694b19bdd2f6e7e37b4895fc5c
darken <darken@darken.eu>
2023-03-25 19:28:26 +0100

    What's updog?

--- app/src/main/java/eu/darken/sdmse/common/EasterEggs.kt
+++ app/src/main/java/eu/darken/sdmse/common/EasterEggs.kt
@@ -4,7 +4,7 @@ import androidx.annotation.StringRes
 
 @get:StringRes
 val easterEggProgressMsg: Int
-    get() = when ((0..7).random()) {
+    get() = when ((0..8).random()) {
         0 -> R.string.general_progress_loading_egg_0
         1 -> R.string.general_progress_loading_egg_1
         2 -> R.string.general_progress_loading_egg_2
@@ -13,5 +13,6 @@ val easterEggProgressMsg: Int
         5 -> R.string.general_progress_loading_egg_5
         6 -> R.string.general_progress_loading_egg_6
         7 -> R.string.general_progress_loading_egg_7
+        8 -> R.string.general_progress_loading_egg_8
         else -> throw IllegalArgumentException()
     }
