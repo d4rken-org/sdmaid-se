de89e3ac4a28a74cc485bcd2cc9e73279e52f1b3
darken <darken@darken.eu>
2023-03-26 17:04:18 +0200

    Code fluff & cleanup

--- app/src/main/java/eu/darken/sdmse/common/debug/DebugCardProvider.kt
+++ app/src/main/java/eu/darken/sdmse/common/debug/DebugCardProvider.kt
@@ -1,15 +1,13 @@
 package eu.darken.sdmse.common.debug
 
-import eu.darken.sdmse.appcleaner.core.automation.ClearCacheTask
 import eu.darken.sdmse.automation.core.AutomationController
 import eu.darken.sdmse.common.areas.DataAreaManager
 import eu.darken.sdmse.common.coroutine.AppScope
 import eu.darken.sdmse.common.datastore.valueBlocking
 import eu.darken.sdmse.common.debug.autoreport.DebugSettings
+import eu.darken.sdmse.common.files.core.GatewaySwitch
 import eu.darken.sdmse.common.navigation.navVia
 import eu.darken.sdmse.common.pkgs.PkgRepo
-import eu.darken.sdmse.common.pkgs.container.NormalPkg
-import eu.darken.sdmse.common.pkgs.currentPkgs
 import eu.darken.sdmse.common.pkgs.pkgops.PkgOps
 import eu.darken.sdmse.common.uix.ViewModel3
 import eu.darken.sdmse.main.ui.dashboard.DashboardFragmentDirections
@@ -17,16 +15,16 @@ import eu.darken.sdmse.main.ui.dashboard.items.DebugCardVH
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class DebugCardProvider @Inject constructor(
     @AppScope private val appScope: CoroutineScope,
     private val debugSettings: DebugSettings,
     private val pkgRepo: PkgRepo,
-    private val pkgOps: PkgOps,
     private val dataAreaManager: DataAreaManager,
+    private val pkgOps: PkgOps,
     private val automationController: AutomationController,
+    private val gatewaySwitch: GatewaySwitch,
 ) {
 
     fun create(vm: ViewModel3) = combine(
@@ -49,11 +47,8 @@ class DebugCardProvider @Inject constructor(
                 }
             },
             onRunTest = {
-                appScope.launch {
-                    val pkgs = pkgRepo.currentPkgs()
-                        .filter { it is NormalPkg }
-                        .map { it.id }
-                    automationController.submit(ClearCacheTask(pkgs))
+                vm.launch {
+
                 }
             },
             onViewLog = {
