b51db6cf2a59788b28702b2e3b89ea1badb6fb12
darken <darken@darken.eu>
2023-03-27 14:59:37 +0200

    Add root test options to debug card

--- app/src/main/java/eu/darken/sdmse/common/debug/DebugCardProvider.kt
+++ app/src/main/java/eu/darken/sdmse/common/debug/DebugCardProvider.kt
@@ -3,23 +3,33 @@ package eu.darken.sdmse.common.debug
 import eu.darken.sdmse.automation.core.AutomationController
 import eu.darken.sdmse.common.areas.DataAreaManager
 import eu.darken.sdmse.common.coroutine.AppScope
+import eu.darken.sdmse.common.datastore.value
 import eu.darken.sdmse.common.datastore.valueBlocking
 import eu.darken.sdmse.common.debug.autoreport.DebugSettings
 import eu.darken.sdmse.common.files.core.GatewaySwitch
 import eu.darken.sdmse.common.navigation.navVia
 import eu.darken.sdmse.common.pkgs.PkgRepo
 import eu.darken.sdmse.common.pkgs.pkgops.PkgOps
+import eu.darken.sdmse.common.root.RootManager
+import eu.darken.sdmse.common.root.RootSettings
+import eu.darken.sdmse.common.root.javaroot.JavaRootClient
 import eu.darken.sdmse.common.uix.ViewModel3
 import eu.darken.sdmse.main.ui.dashboard.DashboardFragmentDirections
 import eu.darken.sdmse.main.ui.dashboard.items.DebugCardVH
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.withTimeoutOrNull
+import java.util.*
 import javax.inject.Inject
 
 class DebugCardProvider @Inject constructor(
     @AppScope private val appScope: CoroutineScope,
     private val debugSettings: DebugSettings,
+    private val rootSettings: RootSettings,
+    private val rootManager: RootManager,
+    private val rootClient: JavaRootClient,
     private val pkgRepo: PkgRepo,
     private val dataAreaManager: DataAreaManager,
     private val pkgOps: PkgOps,
@@ -27,11 +37,14 @@ class DebugCardProvider @Inject constructor(
     private val gatewaySwitch: GatewaySwitch,
 ) {
 
+    private val rootTestState = MutableStateFlow<RootTestResult?>(null)
+
     fun create(vm: ViewModel3) = combine(
         debugSettings.isDebugMode.flow.distinctUntilChanged(),
         debugSettings.isTraceMode.flow.distinctUntilChanged(),
         debugSettings.isDryRunMode.flow.distinctUntilChanged(),
-    ) { isDebug, isTrace, isDryRun ->
+        rootTestState
+    ) { isDebug, isTrace, isDryRun, rootState ->
         if (!isDebug) return@combine null
         DebugCardVH.Item(
             isDryRunEnabled = isDryRun,
@@ -53,7 +66,27 @@ class DebugCardProvider @Inject constructor(
             },
             onViewLog = {
                 DashboardFragmentDirections.actionDashboardFragmentToLogViewFragment().navVia(vm)
+            },
+            rootTestResult = rootState,
+            onTestRoot = {
+                vm.launch {
+                    rootTestState.value = RootTestResult(
+                        testId = UUID.randomUUID().toString(),
+                        allowed = rootSettings.useRoot.value(),
+                        magiskGranted = rootManager.isRooted(),
+                        serviceLaunched = withTimeoutOrNull(10 * 1000) {
+                            rootClient.runSessionAction { it.ipc.checkBase() }
+                        }
+                    )
+                }
             }
         )
     }
+
+    data class RootTestResult(
+        val testId: String,
+        val allowed: Boolean?,
+        val magiskGranted: Boolean,
+        val serviceLaunched: String?,
+    )
 }
