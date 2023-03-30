134bc26c85588d16a68794e2657ab6e075b1018e
darken <darken@darken.eu>
2023-03-30 15:37:28 +0200

    SystemCleaner: Faster cancellation and detailed errors if a filter fails.

---
+++ app/src/main/java/eu/darken/sdmse/systemcleaner/core/filter/SystemCleanerFilterException.kt
@@ -0,0 +1,26 @@
+package eu.darken.sdmse.systemcleaner.core.filter
+
+import eu.darken.sdmse.R
+import eu.darken.sdmse.common.ca.caString
+import eu.darken.sdmse.common.ca.toCaString
+import eu.darken.sdmse.common.error.HasLocalizedError
+import eu.darken.sdmse.common.error.LocalizedError
+import eu.darken.sdmse.common.error.localized
+
+class SystemCleanerFilterException(
+    val filter: SystemCleanerFilter,
+    override val cause: Throwable
+) : Exception(), HasLocalizedError {
+
+    override val message: String
+        get() = "SystemCleaner filter failed: $filter"
+
+    override fun getLocalizedError(): LocalizedError = LocalizedError(
+        throwable = this,
+        label = R.string.general_error_label.toCaString(),
+        description = caString {
+            val subReason = cause.localized(it).asText().get(it)
+            "SystemCleaner filter failed: $filter.\n\n$subReason"
+        }
+    )
+}
