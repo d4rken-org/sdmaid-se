edb0002f998fd1137e1e95c0eec17389941b8ef2
darken <darken@darken.eu>
2023-04-01 01:49:28 +0200

    AppControl: Add list filter options for different tags.

---
+++ app/src/main/java/eu/darken/sdmse/appcontrol/core/SortSettings.kt
@@ -0,0 +1,19 @@
+package eu.darken.sdmse.appcontrol.core
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SortSettings(
+    @Json(name = "mode") val mode: Mode = Mode.LAST_UPDATE,
+    @Json(name = "reversed") val reversed: Boolean = false,
+) {
+    @JsonClass(generateAdapter = false)
+    enum class Mode {
+        @Json(name = "NAME") NAME,
+        @Json(name = "LAST_UPDATE") LAST_UPDATE,
+        @Json(name = "INSTALLED_AT") INSTALLED_AT,
+        @Json(name = "PACKAGENAME") PACKAGENAME,
+        ;
+    }
+}
--- app/src/main/java/eu/darken/sdmse/appcontrol/ui/list/AppControlListFragmentVM.kt
+++ app/src/main/java/eu/darken/sdmse/appcontrol/ui/list/AppControlListFragmentVM.kt
@@ -5,14 +5,16 @@ import android.content.Context
 import androidx.lifecycle.SavedStateHandle
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
-import eu.darken.sdmse.appcontrol.core.AppControl
-import eu.darken.sdmse.appcontrol.core.AppInfo
+import eu.darken.sdmse.appcontrol.core.*
 import eu.darken.sdmse.appcontrol.core.tasks.AppControlScanTask
 import eu.darken.sdmse.common.SingleLiveEvent
 import eu.darken.sdmse.common.coroutine.DispatcherProvider
 import eu.darken.sdmse.common.debug.logging.log
 import eu.darken.sdmse.common.debug.logging.logTag
+import eu.darken.sdmse.common.flow.combine
 import eu.darken.sdmse.common.pkgs.features.ExtendedInstallData
+import eu.darken.sdmse.common.pkgs.isEnabled
+import eu.darken.sdmse.common.pkgs.isSystemApp
 import eu.darken.sdmse.common.progress.Progress
 import eu.darken.sdmse.common.uix.ViewModel3
 import eu.darken.sdmse.main.core.taskmanager.TaskManager
@@ -28,6 +30,7 @@ class AppControlListFragmentVM @Inject constructor(
     @ApplicationContext private val context: Context,
     private val appControl: AppControl,
     private val taskManager: TaskManager,
+    private val settings: AppControlSettings,
 ) : ViewModel3(dispatcherProvider) {
 
     init {
@@ -40,39 +43,45 @@ class AppControlListFragmentVM @Inject constructor(
 
     val events = SingleLiveEvent<AppControlListEvents>()
     private val searchQuery = MutableStateFlow("")
-    private val sortMode = MutableStateFlow(State.SortMode.NAME)
-    private val sortReversed = MutableStateFlow(false)
 
     val state = combine(
         appControl.data,
         appControl.progress,
         searchQuery,
-        sortMode,
-        sortReversed,
-    ) { data, progress, query, sortMode, sortReversed ->
+        settings.listSort.flow,
+        settings.listFilter.flow,
+    ) { data, progress, query, listSort, listFilter ->
         val appInfos = data?.apps
+            ?.filter {
+                if (listFilter.tags.contains(FilterSettings.Tag.USER) && it.pkg.isSystemApp) return@filter false
+                if (listFilter.tags.contains(FilterSettings.Tag.SYSTEM) && !it.pkg.isSystemApp) return@filter false
+                if (listFilter.tags.contains(FilterSettings.Tag.ENABLED) && !it.pkg.isEnabled) return@filter false
+                if (listFilter.tags.contains(FilterSettings.Tag.DISABLED) && it.pkg.isEnabled) return@filter false
+
+                return@filter true
+            }
             ?.filter {
                 if (query.isEmpty()) return@filter true
                 it.pkg.packageName.contains(query)
             }
             ?.sortedWith(
-                when (sortMode) {
-                    State.SortMode.NAME -> compareBy<AppInfo> {
+                when (listSort.mode) {
+                    SortSettings.Mode.NAME -> compareBy<AppInfo> {
                         it.label.get(context).uppercase()
                     }
-                    State.SortMode.PACKAGENAME -> compareBy<AppInfo> {
+                    SortSettings.Mode.PACKAGENAME -> compareBy<AppInfo> {
                         it.pkg.packageName.uppercase()
                     }
-                    State.SortMode.LAST_UPDATE -> compareBy<AppInfo> {
+                    SortSettings.Mode.LAST_UPDATE -> compareBy<AppInfo> {
                         (it.pkg as? ExtendedInstallData)?.updatedAt ?: Instant.EPOCH
                     }
-                    State.SortMode.INSTALLED_AT -> compareBy<AppInfo> {
+                    SortSettings.Mode.INSTALLED_AT -> compareBy<AppInfo> {
                         (it.pkg as? ExtendedInstallData)?.installedAt ?: Instant.EPOCH
                     }
                 }
             )
             ?.let {
-                if (sortReversed) it.reversed() else it
+                if (listSort.reversed) it.reversed() else it
             }
             ?.map { content ->
                 AppControlListRowVH.Item(
@@ -85,12 +94,13 @@ class AppControlListFragmentVM @Inject constructor(
                 )
             }
             ?.toList()
+
         State(
             appInfos = appInfos,
             progress = progress,
-            sortReversed = sortReversed,
-            sortMode = sortMode,
             searchQuery = query,
+            listSort = listSort,
+            listFilter = listFilter,
         )
     }.asLiveData2()
 
@@ -99,30 +109,57 @@ class AppControlListFragmentVM @Inject constructor(
         searchQuery.value = query
     }
 
-    fun updateSortMode(mode: State.SortMode) {
+    fun updateSortMode(mode: SortSettings.Mode) = launch {
         log(TAG) { "updateSortMode($mode)" }
-        sortMode.value = mode
+        settings.listSort.update {
+            it.copy(mode = mode)
+        }
     }
 
-    fun toggleSortDirection() {
+    fun toggleSortDirection() = launch {
         log(TAG) { "toggleSortDirection()" }
-        sortReversed.value = !sortReversed.value
+        settings.listSort.update {
+            it.copy(reversed = !it.reversed)
+        }
+    }
+
+    fun toggleTag(tag: FilterSettings.Tag) = launch {
+        log(TAG) { "toggleTag($tag)" }
+        settings.listFilter.update { old ->
+            val existing = old.tags.contains(tag)
+            val newTags = when (tag) {
+                FilterSettings.Tag.USER -> if (existing) {
+                    old.tags.minus(tag)
+                } else {
+                    old.tags.plus(tag).minus(FilterSettings.Tag.SYSTEM)
+                }
+                FilterSettings.Tag.SYSTEM -> if (existing) {
+                    old.tags.minus(tag)
+                } else {
+                    old.tags.plus(tag).minus(FilterSettings.Tag.USER)
+                }
+                FilterSettings.Tag.ENABLED -> if (existing) {
+                    old.tags.minus(tag)
+                } else {
+                    old.tags.plus(tag).minus(FilterSettings.Tag.DISABLED)
+                }
+                FilterSettings.Tag.DISABLED -> if (existing) {
+                    old.tags.minus(tag)
+                } else {
+                    old.tags.plus(tag).minus(FilterSettings.Tag.ENABLED)
+                }
+            }
+            old.copy(tags = newTags)
+        }
     }
 
     data class State(
         val appInfos: List<AppControlListRowVH.Item>?,
         val progress: Progress.Data?,
-        val sortReversed: Boolean,
-        val sortMode: SortMode,
         val searchQuery: String,
-    ) {
-        enum class SortMode {
-            NAME,
-            LAST_UPDATE,
-            INSTALLED_AT,
-            PACKAGENAME
-        }
-    }
+        val listSort: SortSettings,
+        val listFilter: FilterSettings,
+    )
 
     companion object {
         private val TAG = logTag("AppControl", "List", "VM")
