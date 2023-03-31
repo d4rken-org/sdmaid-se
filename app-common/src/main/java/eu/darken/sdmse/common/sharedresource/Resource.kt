b81f488a65b866fa0dcf0ed881a85af778d40969
darken <darken@darken.eu>
2023-03-31 12:44:01 +0200

    Core: Propagate cancellation exceptions when accessing shared resources. When canceling tasks, and thus canceling shared operations, the resources accessor could get an IllegalStateException which should instead be a CancellationException that doesn't warrant further handling. +Improve logging in trace mode.

--- app-common/src/main/java/eu/darken/sdmse/common/sharedresource/Resource.kt
+++ app-common/src/main/java/eu/darken/sdmse/common/sharedresource/Resource.kt
@@ -1,9 +1,15 @@
 package eu.darken.sdmse.common.sharedresource
 
-data class Resource<T>(private val _item: T, val keepAlive: KeepAlive) : KeepAlive by keepAlive {
+import kotlin.coroutines.cancellation.CancellationException
+
+data class Resource<T>(
+    private val _item: T,
+    val lease: SharedResource<*>.ActiveLease,
+) : KeepAlive by lease {
     val item: T
         get() {
-            check(!keepAlive.isClosed) { "Trying to access closed resource! ($_item, $keepAlive)" }
+            if (lease.job.isCancelled) throw CancellationException("Resource was cancelled! ($_item, $lease)")
+            if (lease.isClosed) throw IllegalAccessException("Trying to access closed resource! ($_item, $lease)")
             return _item
         }
 }
