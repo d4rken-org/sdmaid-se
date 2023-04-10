48ed6a540871e4c0625ee87ac7d1e04f8ad0b3b3
darken <darken@darken.eu>
2023-04-10 17:01:04 +0200

    Handle errors from TaskManager when launched outside of the UI (i.e. scheduler,watcher)

--- app/src/main/java/eu/darken/sdmse/scheduler/core/SchedulerReceiver.kt
+++ app/src/main/java/eu/darken/sdmse/scheduler/core/SchedulerReceiver.kt
@@ -9,9 +9,11 @@ import eu.darken.sdmse.appcleaner.core.tasks.AppCleanerSchedulerTask
 import eu.darken.sdmse.common.coroutine.AppScope
 import eu.darken.sdmse.common.datastore.value
 import eu.darken.sdmse.common.debug.logging.Logging.Priority.*
+import eu.darken.sdmse.common.debug.logging.asLog
 import eu.darken.sdmse.common.debug.logging.log
 import eu.darken.sdmse.common.debug.logging.logTag
 import eu.darken.sdmse.corpsefinder.core.tasks.CorpseFinderSchedulerTask
+import eu.darken.sdmse.main.core.SDMTool
 import eu.darken.sdmse.main.core.taskmanager.TaskManager
 import eu.darken.sdmse.systemcleaner.core.tasks.SystemCleanerSchedulerTask
 import kotlinx.coroutines.CoroutineScope
@@ -57,21 +59,23 @@ class SchedulerReceiver : BroadcastReceiver() {
                 return@launch
             }
 
-            if (schedule.useCorpseFinder) {
+
+            val tasks = mutableListOf<SDMTool.Task>()
+
+            if (schedule.useCorpseFinder) tasks.add(CorpseFinderSchedulerTask(schedule.id))
+            if (schedule.useSystemCleaner) tasks.add(SystemCleanerSchedulerTask(schedule.id))
+            if (schedule.useAppCleaner) tasks.add(AppCleanerSchedulerTask(schedule.id))
+
+            tasks.forEach { task ->
                 appScope.launch {
-                    taskManager.submit(CorpseFinderSchedulerTask(schedule.id))
+                    try {
+                        taskManager.submit(task)
+                    } catch (e: Exception) {
+                        log(TAG, ERROR) { "Scheduler task failed ($task): ${e.asLog()}" }
+                    }
                 }
             }
-            if (schedule.useSystemCleaner) {
-                appScope.launch {
-                    taskManager.submit(SystemCleanerSchedulerTask(schedule.id))
-                }
-            }
-            if (schedule.useAppCleaner) {
-                appScope.launch {
-                    taskManager.submit(AppCleanerSchedulerTask(schedule.id))
-                }
-            }
+
             schedulerManager.updateExecutedNow(scheduleId)
         }
 
