8c542118b192ec47376c7f4b229288519bb035c7
Matthias Urhahn <darken@darken.eu>
2023-04-12 07:50:47 +0200

    Show names of translators in settings (#157)
    * Add field to show translator names.
    
    * Fix typo

--- app/src/main/java/eu/darken/sdmse/main/ui/settings/acks/AcknowledgementsFragment.kt
+++ app/src/main/java/eu/darken/sdmse/main/ui/settings/acks/AcknowledgementsFragment.kt
@@ -2,8 +2,12 @@ package eu.darken.sdmse.main.ui.settings.acks
 
 import androidx.annotation.Keep
 import androidx.fragment.app.viewModels
+import androidx.preference.Preference
 import dagger.hilt.android.AndroidEntryPoint
 import eu.darken.sdmse.R
+import eu.darken.sdmse.common.debug.logging.Logging.Priority.WARN
+import eu.darken.sdmse.common.debug.logging.asLog
+import eu.darken.sdmse.common.debug.logging.log
 import eu.darken.sdmse.common.uix.PreferenceFragment2
 import eu.darken.sdmse.main.core.GeneralSettings
 import javax.inject.Inject
@@ -18,5 +22,18 @@ class AcknowledgementsFragment : PreferenceFragment2() {
     @Inject lateinit var debugSettings: GeneralSettings
 
     override val settings: GeneralSettings by lazy { debugSettings }
+
+    override fun onPreferencesChanged() {
+        super.onPreferencesChanged()
+        findPreference<Preference>("translation.translators")!!.apply {
+            val peopleRaw = getString(R.string.translation_translators_people)
+            summary = try {
+                peopleRaw.split(";").joinToString("\n")
+            } catch (e: Exception) {
+                log(WARN) { "Translator split failed $peopleRaw: ${e.asLog()}" }
+                peopleRaw
+            }
+        }
+    }
 
 }
