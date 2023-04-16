f6cf1d4b606aef553d1872314ab6a41263b7b4d9
darken <darken@darken.eu>
2023-04-17 01:06:57 +0200

    AppCleaner: When generating search paths, use CSI to find additional owners for blacklist locations, i.e. determine "some.pkg" for "Android/data/_some.pkg".

--- app/src/main/java/eu/darken/sdmse/common/forensics/Owner.kt
+++ app/src/main/java/eu/darken/sdmse/common/forensics/Owner.kt
@@ -3,6 +3,7 @@ package eu.darken.sdmse.common.forensics
 import android.os.Parcelable
 import eu.darken.sdmse.common.clutter.Marker
 import eu.darken.sdmse.common.pkgs.Pkg
+import eu.darken.sdmse.common.pkgs.features.Installed
 import eu.darken.sdmse.common.user.UserHandle2
 import kotlinx.parcelize.Parcelize
 
@@ -12,6 +13,9 @@ data class Owner(
     val userHandle: UserHandle2,
     val flags: Set<Marker.Flag> = emptySet(),
 ) : Parcelable {
+
+    val installId: Installed.InstallId
+        get() = Installed.InstallId(pkgId, userHandle)
 
     fun hasFlag(flag: Marker.Flag): Boolean = flags.contains(flag)
 
