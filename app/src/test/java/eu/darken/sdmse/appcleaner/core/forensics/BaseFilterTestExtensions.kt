fb85afd58222b8b7c69c463335666638692ba955
darken <darken@darken.eu>
2023-05-25 15:14:49 +0200

    AppCleaner: Improved support for deletion of old WhatsApp Business backups.
    Closes #80

--- app/src/test/java/eu/darken/sdmse/appcleaner/core/forensics/BaseFilterTestExtensions.kt
+++ app/src/test/java/eu/darken/sdmse/appcleaner/core/forensics/BaseFilterTestExtensions.kt
@@ -3,6 +3,7 @@ package eu.darken.sdmse.appcleaner.core.forensics
 import eu.darken.sdmse.common.areas.DataArea
 import eu.darken.sdmse.common.pkgs.toPkgId
 import java.io.File
+import java.time.Instant
 
 fun BaseFilterTest.pos(pkg: String, areaType: DataArea.Type, vararg segments: String) {
     BaseFilterTest.Candidate(
@@ -19,5 +20,25 @@ fun BaseFilterTest.neg(pkg: String, areaType: DataArea.Type, vararg segments: St
         pkgs = setOf(pkg.toPkgId()),
         areaTypes = setOf(areaType),
         prefixFreePaths = setOf(segments.map { it.split(File.separatorChar) }.flatten())
+    ).let { addCandidate(it) }
+}
+
+fun BaseFilterTest.pos(pkg: String, areaType: DataArea.Type, lastModified: Instant, vararg segments: String) {
+    BaseFilterTest.Candidate(
+        matchType = BaseFilterTest.Candidate.Type.POSITIVE,
+        pkgs = setOf(pkg.toPkgId()),
+        areaTypes = setOf(areaType),
+        prefixFreePaths = setOf(segments.map { it.split(File.separatorChar) }.flatten()),
+        lastModified = lastModified,
+    ).let { addCandidate(it) }
+}
+
+fun BaseFilterTest.neg(pkg: String, areaType: DataArea.Type, lastModified: Instant, vararg segments: String) {
+    BaseFilterTest.Candidate(
+        matchType = BaseFilterTest.Candidate.Type.NEGATIVE,
+        pkgs = setOf(pkg.toPkgId()),
+        areaTypes = setOf(areaType),
+        prefixFreePaths = setOf(segments.map { it.split(File.separatorChar) }.flatten()),
+        lastModified = lastModified,
     ).let { addCandidate(it) }
 }
