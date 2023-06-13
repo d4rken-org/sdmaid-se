7aac5a931ab70a1ddb2a949de44247259a46b889
darken <darken@darken.eu>
2023-06-13 07:00:20 +0200

    Add data areas to bug report

--- app/src/gplay/java/eu/darken/sdmse/common/debug/autoreport/bugsnag/BugsnagErrorHandler.kt
+++ app/src/gplay/java/eu/darken/sdmse/common/debug/autoreport/bugsnag/BugsnagErrorHandler.kt
@@ -8,12 +8,15 @@ import com.bugsnag.android.Event
 import com.bugsnag.android.OnErrorCallback
 import dagger.hilt.android.qualifiers.ApplicationContext
 import eu.darken.sdmse.common.BuildConfigWrap
+import eu.darken.sdmse.common.areas.DataAreaManager
 import eu.darken.sdmse.common.datastore.valueBlocking
 import eu.darken.sdmse.common.debug.autoreport.DebugSettings
 import eu.darken.sdmse.common.debug.logging.Logging.Priority.WARN
 import eu.darken.sdmse.common.debug.logging.asLog
 import eu.darken.sdmse.common.debug.logging.log
 import eu.darken.sdmse.main.core.GeneralSettings
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -23,12 +26,12 @@ class BugsnagErrorHandler @Inject constructor(
     private val bugsnagLogger: BugsnagLogger,
     private val generalSettings: GeneralSettings,
     private val debugSettings: DebugSettings,
+    private val dataAreaManager: DataAreaManager,
 ) : OnErrorCallback {
 
     override fun onError(event: Event): Boolean {
-        bugsnagLogger.injectLog(event)
 
-        TAB_APP.also { tab ->
+        TAB_APP.let { tab ->
             event.addMetadata(tab, "flavor", BuildConfigWrap.FLAVOR.toString())
             event.addMetadata(tab, "buildType", BuildConfigWrap.BUILD_TYPE.toString())
 
@@ -38,11 +41,28 @@ class BugsnagErrorHandler @Inject constructor(
             context.tryFormattedSignature()?.let { event.addMetadata(tab, "signatures", it) }
         }
 
+        TAB_DATA_AREAS.let { tab ->
+            runBlocking { dataAreaManager.latestState.first() }
+                ?.areas
+                ?.groupBy { it.type }
+                ?.forEach { (type, areas) ->
+                    areas.forEachIndexed { index, area ->
+                        val key = "${type.raw} #$index"
+                        val row = "${area.path} | ${area.userHandle.handleId} | ${area.flags}"
+
+                        event.addMetadata(tab, key, row)
+                    }
+                }
+        }
+
+        bugsnagLogger.injectLog(event)
+
         return generalSettings.isBugReporterEnabled.valueBlocking && !BuildConfigWrap.DEBUG
     }
 
     companion object {
         private const val TAB_APP = "app"
+        private const val TAB_DATA_AREAS = "data areas"
 
         @Suppress("DEPRECATION")
         @SuppressLint("PackageManagerGetSignatures")
