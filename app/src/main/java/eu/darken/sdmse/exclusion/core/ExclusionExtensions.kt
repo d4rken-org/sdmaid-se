53aae05488e7e5ba8fc378339660af36c92a0125
darken <darken@darken.eu>
2023-07-07 21:55:04 +0200

    AppCleaner: Fix exclusion matching. In some cases we removed more results than necessary and in other cases we didn't remove enough (parents of nested exclusion targets).

--- app/src/main/java/eu/darken/sdmse/exclusion/core/ExclusionExtensions.kt
+++ app/src/main/java/eu/darken/sdmse/exclusion/core/ExclusionExtensions.kt
@@ -1,5 +1,8 @@
 package eu.darken.sdmse.exclusion.core
 
+import eu.darken.sdmse.common.debug.logging.Logging.Priority.VERBOSE
+import eu.darken.sdmse.common.debug.logging.log
+import eu.darken.sdmse.common.debug.logging.logTag
 import eu.darken.sdmse.common.files.APath
 import eu.darken.sdmse.common.files.APathLookup
 import eu.darken.sdmse.common.files.isAncestorOf
@@ -34,38 +37,51 @@ suspend fun ExclusionManager.pkgExclusions(tool: SDMTool.Type) = currentExclusio
         }
     }
 
-suspend fun <P : APath, PL : APathLookup<P>> Exclusion.Path.excludeNestedLookups(paths: Collection<PL>): Collection<PL> {
-    if (paths.isEmpty()) return emptySet()
 
-    val excluded = mutableSetOf<PL>()
+suspend fun <P : APath, PL : APathLookup<P>> Exclusion.Path.excludeNestedLookups(paths: Collection<PL>): Set<PL> {
+    val pathMap = paths.associateBy { it.lookedUp }
 
-    var temp = paths.filter { path ->
-        match(path.lookedUp).also {
-            if (it) excluded.add(path)
-        }
-    }
+    val result = this.excludeNested(pathMap.keys)
 
-    temp = temp.filter { path ->
-        excluded.none { path.isAncestorOf(it) }
-    }
+    return result.map { pathMap[it]!! }.toSet()
+}
 
+suspend fun <P : APath, PL : APathLookup<P>> Collection<Exclusion.Path>.excludeNestedLookups(paths: Collection<PL>): Set<PL> {
+    var temp = paths.toSet()
+    this.forEach { temp = it.excludeNestedLookups(temp) }
     return temp
 }
 
-suspend fun <T : APath> Exclusion.Path.excludeNested(paths: Collection<T>): Collection<T> {
+suspend fun <T : APath> Collection<Exclusion.Path>.excludeNested(paths: Collection<T>): Set<T> {
+    var temp = paths.toSet()
+    this.forEach { temp = it.excludeNested(temp) }
+    return temp
+}
+
+suspend fun <T : APath> Exclusion.Path.excludeNested(paths: Collection<T>): Set<T> {
     if (paths.isEmpty()) return emptySet()
 
     val excluded = mutableSetOf<T>()
 
-    var temp = paths.filter { path ->
-        match(path).also {
-            if (it) excluded.add(path)
-        }
+    // Files that are left after applying direct exclusions
+    val afterFirstPass = paths.filter { path ->
+        val isExcluded = match(path)
+        if (isExcluded) excluded.add(path)
+        !isExcluded
     }
 
-    temp = temp.filter { path ->
-        excluded.none { path.isAncestorOf(it) }
+    // Files that are left after also checking ancestors, i.e. if we have:
+    // dirA
+    // dirA/dirB/
+    // dirA/dirB/file
+    // and exclude "file", then we also need to exclude all "dirA" and "dirB", otherwise we delete the parent and thus "file"
+    val afterSecondPass = afterFirstPass.filterNot { path ->
+        val reason = excluded.firstOrNull { path.isAncestorOf(it) }
+        if (reason != null) log(TAG, VERBOSE) { "Nested exclusion match: $path <- $reason <- $this" }
+        reason != null
     }
 
-    return temp
+    return afterSecondPass.toSet()
 }
+
+private val TAG = logTag("Exclusion", "Extensions")
