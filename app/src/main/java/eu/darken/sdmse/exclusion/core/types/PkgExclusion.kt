399d471c79fe91014cbbf9fef0ef81f6695fac13
darken <darken@darken.eu>
2023-07-07 21:55:13 +0200

    Add debug output to exclusions

--- app/src/main/java/eu/darken/sdmse/exclusion/core/types/PkgExclusion.kt
+++ app/src/main/java/eu/darken/sdmse/exclusion/core/types/PkgExclusion.kt
@@ -4,6 +4,9 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import eu.darken.sdmse.common.ca.CaString
 import eu.darken.sdmse.common.ca.caString
+import eu.darken.sdmse.common.debug.logging.Logging.Priority.VERBOSE
+import eu.darken.sdmse.common.debug.logging.log
+import eu.darken.sdmse.common.debug.logging.logTag
 import eu.darken.sdmse.common.pkgs.Pkg
 import eu.darken.sdmse.common.pkgs.getLabel2
 
@@ -20,10 +23,13 @@ data class PkgExclusion(
         get() = caString { it.packageManager.getLabel2(pkgId) ?: pkgId.name }
 
     override suspend fun match(candidate: Pkg.Id): Boolean {
-        return pkgId == candidate
+        val match = pkgId == candidate
+        if (match) log(TAG, VERBOSE) { "Exclusion match: $candidate <- $this " }
+        return match
     }
 
     companion object {
         fun createId(pkgId: Pkg.Id): ExclusionId = "${PkgExclusion::class.simpleName}-${pkgId.name}"
+        private val TAG = logTag("Exclusion", "Pkg")
     }
 }
--- app/src/main/java/eu/darken/sdmse/exclusion/core/types/SegmentExclusion.kt
+++ app/src/main/java/eu/darken/sdmse/exclusion/core/types/SegmentExclusion.kt
@@ -4,6 +4,9 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import eu.darken.sdmse.common.ca.CaString
 import eu.darken.sdmse.common.ca.caString
+import eu.darken.sdmse.common.debug.logging.Logging.Priority.VERBOSE
+import eu.darken.sdmse.common.debug.logging.log
+import eu.darken.sdmse.common.debug.logging.logTag
 import eu.darken.sdmse.common.files.APath
 import eu.darken.sdmse.common.files.Segments
 import eu.darken.sdmse.common.files.containsSegments
@@ -23,14 +26,20 @@ data class SegmentExclusion(
     override val label: CaString
         get() = caString { segments.joinSegments() }
 
-    override suspend fun match(candidate: APath): Boolean = candidate.segments.containsSegments(
-        other = this.segments,
-        allowPartial = allowPartial,
-        ignoreCase = ignoreCase,
-    )
+    override suspend fun match(candidate: APath): Boolean {
+        val match = candidate.segments.containsSegments(
+            other = this.segments,
+            allowPartial = allowPartial,
+            ignoreCase = ignoreCase,
+        )
+        if (match) log(TAG, VERBOSE) { "Exclusion match: $candidate <- $this " }
+        return match
+    }
 
     companion object {
         fun createId(segments: Segments): ExclusionId =
             "${SegmentExclusion::class.simpleName}-${segments.joinToString("/")}"
+
+        private val TAG = logTag("Exclusion", "Segment")
     }
 }
