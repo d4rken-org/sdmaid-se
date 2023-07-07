e6149c60e38baf37709faf52b801ddb6d4903138
darken <darken@darken.eu>
2023-07-07 21:55:13 +0200

    Add debug output to exclusions

--- app/src/main/java/eu/darken/sdmse/exclusion/core/types/PathExclusion.kt
+++ app/src/main/java/eu/darken/sdmse/exclusion/core/types/PathExclusion.kt
@@ -3,6 +3,9 @@ package eu.darken.sdmse.exclusion.core.types
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import eu.darken.sdmse.common.ca.CaString
+import eu.darken.sdmse.common.debug.logging.Logging.Priority.VERBOSE
+import eu.darken.sdmse.common.debug.logging.log
+import eu.darken.sdmse.common.debug.logging.logTag
 import eu.darken.sdmse.common.files.APath
 import eu.darken.sdmse.common.files.isAncestorOf
 import eu.darken.sdmse.common.files.matches
@@ -20,10 +23,14 @@ data class PathExclusion(
         get() = path.userReadablePath
 
     override suspend fun match(candidate: APath): Boolean {
-        return candidate.matches(path) || path.isAncestorOf(candidate)
+        val match = candidate.matches(path) || path.isAncestorOf(candidate)
+        if (match) log(TAG, VERBOSE) { "Exclusion match: $candidate <- $this " }
+        return match
     }
 
     companion object {
         fun createId(path: APath): ExclusionId = "${PathExclusion::class.simpleName}-${path.path}"
+        private val TAG = logTag("Exclusion", "Path")
+
     }
 }
