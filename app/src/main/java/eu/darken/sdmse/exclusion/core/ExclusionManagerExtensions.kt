1b5c3fb4979f939a6c6a19ab58a3a693e4c3c1be
darken <darken@darken.eu>
2023-07-07 22:51:51 +0200

    SystemCleaner: Improve exclusion matching
    Better handle exclusions that first match during scan, but then other "parent" folders of an exclusion still pass and need to be removed from the final filter contents too.

--- app/src/main/java/eu/darken/sdmse/exclusion/core/ExclusionManagerExtensions.kt
+++ app/src/main/java/eu/darken/sdmse/exclusion/core/ExclusionManagerExtensions.kt
@@ -2,6 +2,8 @@ package eu.darken.sdmse.exclusion.core
 
 import eu.darken.sdmse.exclusion.core.types.Exclusion
 import eu.darken.sdmse.exclusion.core.types.ExclusionId
+import eu.darken.sdmse.exclusion.core.types.hasTags
+import eu.darken.sdmse.main.core.SDMTool
 import kotlinx.coroutines.flow.first
 
 suspend fun ExclusionManager.exists(exclusion: Exclusion) = exists(exclusion.id)
@@ -10,3 +12,29 @@ suspend fun ExclusionManager.exists(exclusionId: ExclusionId) = exclusions.first
 
 suspend fun ExclusionManager.save(exclusion: Exclusion) = save(setOf(exclusion))
 suspend fun ExclusionManager.remove(id: ExclusionId) = remove(setOf(id))
+
+suspend fun ExclusionManager.currentExclusions() = exclusions.first()
+
+suspend fun ExclusionManager.pathExclusions(tool: SDMTool.Type) = currentExclusions()
+    .filterIsInstance<Exclusion.Path>()
+    .filter {
+        return@filter when (tool) {
+            SDMTool.Type.CORPSEFINDER -> it.hasTags(Exclusion.Tag.CORPSEFINDER)
+            SDMTool.Type.SYSTEMCLEANER -> it.hasTags(Exclusion.Tag.SYSTEMCLEANER)
+            SDMTool.Type.APPCLEANER -> it.hasTags(Exclusion.Tag.APPCLEANER)
+            SDMTool.Type.APPCONTROL -> throw UnsupportedOperationException()
+            SDMTool.Type.ANALYZER -> throw UnsupportedOperationException()
+        }
+    }
+
+suspend fun ExclusionManager.pkgExclusions(tool: SDMTool.Type) = currentExclusions()
+    .filterIsInstance<Exclusion.Pkg>()
+    .filter {
+        return@filter when (tool) {
+            SDMTool.Type.CORPSEFINDER -> it.hasTags(Exclusion.Tag.CORPSEFINDER)
+            SDMTool.Type.SYSTEMCLEANER -> it.hasTags(Exclusion.Tag.SYSTEMCLEANER)
+            SDMTool.Type.APPCLEANER -> it.hasTags(Exclusion.Tag.APPCLEANER)
+            SDMTool.Type.APPCONTROL -> throw UnsupportedOperationException()
+            SDMTool.Type.ANALYZER -> throw UnsupportedOperationException()
+        }
+    }
--- app/src/main/java/eu/darken/sdmse/exclusion/core/types/ExclusionExtensions.kt
+++ app/src/main/java/eu/darken/sdmse/exclusion/core/types/ExclusionExtensions.kt
@@ -1,8 +1,61 @@
 package eu.darken.sdmse.exclusion.core.types
 
+import eu.darken.sdmse.common.debug.logging.Logging.Priority.VERBOSE
+import eu.darken.sdmse.common.debug.logging.log
+import eu.darken.sdmse.common.debug.logging.logTag
+import eu.darken.sdmse.common.files.APath
 import eu.darken.sdmse.common.files.APathLookup
+import eu.darken.sdmse.common.files.isAncestorOf
 
 suspend fun Exclusion.Path.match(candidate: APathLookup<*>): Boolean = match(candidate.lookedUp)
 
 fun Exclusion.hasTags(vararg tags: Exclusion.Tag) = this.tags.contains(Exclusion.Tag.GENERAL)
         || tags.any { this.tags.contains(it) }
+
+suspend fun <P : APath, PL : APathLookup<P>> Exclusion.Path.excludeNestedLookups(paths: Collection<PL>): Set<PL> {
+    val pathMap = paths.associateBy { it.lookedUp }
+
+    val result = this.excludeNested(pathMap.keys)
+
+    return result.map { pathMap[it]!! }.toSet()
+}
+
+suspend fun <P : APath, PL : APathLookup<P>> Collection<Exclusion.Path>.excludeNestedLookups(paths: Collection<PL>): Set<PL> {
+    var temp = paths.toSet()
+    this.forEach { temp = it.excludeNestedLookups(temp) }
+    return temp
+}
+
+suspend fun <T : APath> Collection<Exclusion.Path>.excludeNested(paths: Collection<T>): Set<T> {
+    var temp = paths.toSet()
+    this.forEach { temp = it.excludeNested(temp) }
+    return temp
+}
+
+suspend fun <T : APath> Exclusion.Path.excludeNested(paths: Collection<T>): Set<T> {
+    if (paths.isEmpty()) return emptySet()
+
+    val excluded = mutableSetOf<T>()
+
+    // Files that are left after applying direct exclusions
+    val afterFirstPass = paths.filter { path ->
+        val isExcluded = match(path)
+        if (isExcluded) excluded.add(path)
+        !isExcluded
+    }
+
+    // Files that are left after also checking ancestors, i.e. if we have:
+    // dirA
+    // dirA/dirB/
+    // dirA/dirB/file
+    // and exclude "file", then we also need to exclude all "dirA" and "dirB", otherwise we delete the parent and thus "file"
+    val afterSecondPass = afterFirstPass.filterNot { path ->
+        val reason = excluded.firstOrNull { path.isAncestorOf(it) }
+        if (reason != null) log(TAG, VERBOSE) { "Nested exclusion match: $path <- $reason <- $this" }
+        reason != null
+    }
+
+    return afterSecondPass.toSet()
+}
+
+private val TAG = logTag("Exclusion", "Extensions")
