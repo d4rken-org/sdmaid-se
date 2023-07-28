4a380c730afa57baccc7303f0c22aca61cf3f879
darken <darken@darken.eu>
2023-07-28 14:41:06 +0200

    UI: Theming options are a "Pro" feature.

--- app/src/main/java/eu/darken/sdmse/main/ui/settings/general/GeneralSettingsFragment.kt
+++ app/src/main/java/eu/darken/sdmse/main/ui/settings/general/GeneralSettingsFragment.kt
@@ -1,13 +1,16 @@
 package eu.darken.sdmse.main.ui.settings.general
 
+import android.os.Bundle
+import android.view.View
 import androidx.annotation.Keep
 import androidx.fragment.app.viewModels
-import androidx.preference.ListPreference
 import androidx.preference.Preference
 import dagger.hilt.android.AndroidEntryPoint
 import eu.darken.sdmse.MainDirections
 import eu.darken.sdmse.R
 import eu.darken.sdmse.common.BuildConfigWrap
+import eu.darken.sdmse.common.observe2
+import eu.darken.sdmse.common.preferences.ListPreference2
 import eu.darken.sdmse.common.preferences.setupWithEnum
 import eu.darken.sdmse.common.uix.PreferenceFragment2
 import eu.darken.sdmse.main.core.GeneralSettings
@@ -24,8 +27,10 @@ class GeneralSettingsFragment : PreferenceFragment2() {
     override val settings: GeneralSettings by lazy { generalSettings }
     override val preferenceFile: Int = R.xml.preferences_general
 
-    private val themeModePref by lazy { findPreference<ListPreference>(settings.themeMode.keyName)!! }
-    private val themeStylePref by lazy { findPreference<ListPreference>(settings.themeStyle.keyName)!! }
+    private val themeModePref: ListPreference2
+        get() = findPreference(settings.themeMode.keyName)!!
+    private val themeStylePref: ListPreference2
+        get() = findPreference(settings.themeStyle.keyName)!!
 
     override fun onPreferencesCreated() {
         themeModePref.setupWithEnum(settings.themeMode)
@@ -34,10 +39,37 @@ class GeneralSettingsFragment : PreferenceFragment2() {
         findPreference<Preference>(
             "core.bugreporter.enabled"
         )?.isVisible = BuildConfigWrap.FLAVOR != BuildConfigWrap.Flavor.FOSS
+
         findPreference<Preference>("upgrade")?.setOnPreferenceClickListener {
             MainDirections.goToUpgradeFragment(forced = true).navigate()
             true
         }
+
         super.onPreferencesCreated()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        vm.isPro.observe2(this) { isPro ->
+            themeModePref.alternativeClickListener = when {
+                isPro -> null
+
+                else -> {
+                    {
+                        MainDirections.goToUpgradeFragment(forced = true).navigate()
+                    }
+                }
+            }
+            themeStylePref.alternativeClickListener = when {
+                isPro -> null
+
+                else -> {
+                    {
+                        MainDirections.goToUpgradeFragment(forced = true).navigate()
+                    }
+                }
+            }
+        }
+
+        super.onViewCreated(view, savedInstanceState)
     }
 }
--- app/src/main/java/eu/darken/sdmse/main/ui/settings/general/GeneralSettingsViewModel.kt
+++ app/src/main/java/eu/darken/sdmse/main/ui/settings/general/GeneralSettingsViewModel.kt
@@ -5,14 +5,18 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import eu.darken.sdmse.common.coroutine.DispatcherProvider
 import eu.darken.sdmse.common.debug.logging.logTag
 import eu.darken.sdmse.common.uix.ViewModel3
+import eu.darken.sdmse.common.upgrade.UpgradeRepo
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class GeneralSettingsViewModel @Inject constructor(
     private val handle: SavedStateHandle,
     private val dispatcherProvider: DispatcherProvider,
+    private val upgradeRepo: UpgradeRepo
 ) : ViewModel3(dispatcherProvider) {
 
+    val isPro = upgradeRepo.upgradeInfo.map { it.isPro }.asLiveData2()
 
     companion object {
         private val TAG = logTag("Settings", "General", "ViewModel")
