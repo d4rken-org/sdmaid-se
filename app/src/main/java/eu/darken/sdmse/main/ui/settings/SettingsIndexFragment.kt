6af83ce893acac7ae1704affcffa0bff59434df4
darken <darken@darken.eu>
2023-08-08 21:20:10 +0200

    Hide "Sponsor" option from settings if user has pro upgrade

--- app/src/main/java/eu/darken/sdmse/main/ui/settings/SettingsIndexFragment.kt
+++ app/src/main/java/eu/darken/sdmse/main/ui/settings/SettingsIndexFragment.kt
@@ -2,6 +2,7 @@ package eu.darken.sdmse.main.ui.settings
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.preference.Preference
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
@@ -9,10 +10,9 @@ import eu.darken.sdmse.MainDirections
 import eu.darken.sdmse.R
 import eu.darken.sdmse.common.BuildConfigWrap
 import eu.darken.sdmse.common.SdmSeLinks
-import eu.darken.sdmse.common.WebpageTool
 import eu.darken.sdmse.common.datastore.PreferenceScreenData
+import eu.darken.sdmse.common.observe2
 import eu.darken.sdmse.common.uix.PreferenceFragment2
-import eu.darken.sdmse.common.upgrade.UpgradeRepo
 import eu.darken.sdmse.main.core.GeneralSettings
 import javax.inject.Inject
 
@@ -24,16 +24,21 @@ class SettingsIndexFragment : PreferenceFragment2() {
         get() = generalSettings
     override val preferenceFile: Int = R.xml.preferences_index
 
-    @Inject lateinit var webpageTool: WebpageTool
-    @Inject lateinit var upgradeRepo: UpgradeRepo
+    private val vm: SettingsViewModel by viewModels()
+
+    private val sponsorPref: Preference
+        get() = findPreference("core.sponsor.development")!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setupMenu(R.menu.menu_settings_index) { item ->
             when (item.itemId) {
                 R.id.menu_item_twitter -> {
-                    webpageTool.open("https://twitter.com/d4rken")
+                    vm.openWebsite("https://twitter.com/d4rken")
                 }
             }
+        }
+        vm.state.observe2(this) {
+            sponsorPref.isVisible = BuildConfigWrap.FLAVOR == BuildConfigWrap.Flavor.FOSS && !it.isPro
         }
         super.onViewCreated(view, savedInstanceState)
     }
@@ -51,15 +56,12 @@ class SettingsIndexFragment : PreferenceFragment2() {
 
         findPreference<Preference>("core.changelog")!!.summary = BuildConfigWrap.VERSION_DESCRIPTION
         findPreference<Preference>("core.privacy")!!.setOnPreferenceClickListener {
-            webpageTool.open(SdmSeLinks.PRIVACY_POLICY)
+            vm.openWebsite(SdmSeLinks.PRIVACY_POLICY)
             true
         }
-        findPreference<Preference>("core.sponsor.development")?.apply {
-            isVisible = BuildConfigWrap.FLAVOR == BuildConfigWrap.Flavor.FOSS
-            setOnPreferenceClickListener {
-                webpageTool.open(upgradeRepo.mainWebsite)
-                true
-            }
+        sponsorPref.setOnPreferenceClickListener {
+            vm.openUpgradeWebsite()
+            true
         }
 
         findPreference<Preference>("history")!!.setOnPreferenceClickListener {
--- app/src/main/java/eu/darken/sdmse/main/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/eu/darken/sdmse/main/ui/settings/SettingsViewModel.kt
@@ -2,12 +2,33 @@ package eu.darken.sdmse.main.ui.settings
 
 import androidx.lifecycle.SavedStateHandle
 import dagger.hilt.android.lifecycle.HiltViewModel
+import eu.darken.sdmse.common.WebpageTool
 import eu.darken.sdmse.common.coroutine.DispatcherProvider
 import eu.darken.sdmse.common.uix.ViewModel2
+import eu.darken.sdmse.common.upgrade.UpgradeRepo
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
-    private val handle: SavedStateHandle,
-    private val dispatcherProvider: DispatcherProvider,
-) : ViewModel2(dispatcherProvider)
+    @Suppress("unused") val handle: SavedStateHandle,
+    dispatcherProvider: DispatcherProvider,
+    private val upgradeRepo: UpgradeRepo,
+    private val webpageTool: WebpageTool,
+) : ViewModel2(dispatcherProvider) {
+
+    val state = upgradeRepo.upgradeInfo.map { State(it.isPro) }.asLiveData2()
+
+    fun openWebsite(url: String) {
+        webpageTool.open(url)
+    }
+
+    fun openUpgradeWebsite() {
+        webpageTool.open(upgradeRepo.mainWebsite)
+    }
+
+    data class State(
+        val isPro: Boolean
+    )
+
+}
