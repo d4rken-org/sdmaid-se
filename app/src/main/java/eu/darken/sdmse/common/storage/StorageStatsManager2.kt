5056f11c85a4ad53cd015e50e605780f9b876771
darken <darken@darken.eu>
2023-09-22 15:47:29 +0200

    Reduce load on IPC buffer when querying pkg stats.

--- app/src/main/java/eu/darken/sdmse/common/storage/StorageStatsManager2.kt
+++ app/src/main/java/eu/darken/sdmse/common/storage/StorageStatsManager2.kt
@@ -4,17 +4,20 @@ import android.app.usage.StorageStats
 import android.app.usage.StorageStatsManager
 import dagger.Reusable
 import eu.darken.sdmse.common.debug.logging.logTag
+import eu.darken.sdmse.common.funnel.IPCFunnel
 import eu.darken.sdmse.common.pkgs.features.Installed
 import javax.inject.Inject
 
 @Reusable
 class StorageStatsManager2 @Inject constructor(
     private val osStatManager: StorageStatsManager,
+    private val ipcFunnel: IPCFunnel,
 ) {
 
-    suspend fun queryStatsForPkg(storageId: StorageId, pkg: Installed): StorageStats {
+    suspend fun queryStatsForPkg(storageId: StorageId, pkg: Installed): StorageStats = ipcFunnel.use {
+        // We don't need the direct IPCFunnel reference, but to lighten the load in the IPC buffer we use it's locking
         val appUid = pkg.applicationInfo?.uid ?: throw IllegalStateException("${pkg.id} is missing an UID")
-        return osStatManager.queryStatsForUid(storageId.externalId, appUid)
+        osStatManager.queryStatsForUid(storageId.externalId, appUid)
     }
 
     companion object {
