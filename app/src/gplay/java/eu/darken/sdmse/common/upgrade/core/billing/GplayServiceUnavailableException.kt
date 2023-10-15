fcd4203a21cb816fa049e04b1ecb0878889c7547
darken <darken@darken.eu>
2023-10-15 08:10:18 +0200

    Provide the user with more help for `BILLING_UNAVAILABLE` errors

--- app/src/gplay/java/eu/darken/sdmse/common/upgrade/core/billing/GplayServiceUnavailableException.kt
+++ app/src/gplay/java/eu/darken/sdmse/common/upgrade/core/billing/GplayServiceUnavailableException.kt
@@ -1,7 +1,14 @@
 package eu.darken.sdmse.common.upgrade.core.billing
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import android.widget.Toast
 import eu.darken.sdmse.R
 import eu.darken.sdmse.common.ca.toCaString
+import eu.darken.sdmse.common.debug.logging.Logging.Priority.ERROR
+import eu.darken.sdmse.common.debug.logging.log
 import eu.darken.sdmse.common.error.HasLocalizedError
 import eu.darken.sdmse.common.error.LocalizedError
 
@@ -10,8 +17,26 @@ class GplayServiceUnavailableException(cause: Throwable) :
 
     override fun getLocalizedError(): LocalizedError = LocalizedError(
         throwable = this,
-        label = "Google Play Services Unavailable".toCaString(),
-        description = R.string.upgrades_gplay_unavailable_error.toCaString()
+        label = R.string.upgrades_gplay_unavailable_error_title.toCaString(),
+        description = R.string.upgrades_gplay_unavailable_error_description.toCaString(),
+        fixActionLabel = "Google Play".toCaString(),
+        fixAction = { activity ->
+            try {
+                val intent = Intent().apply {
+                    action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                    data = Uri.fromParts("package", GPLAY_PKG, null)
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }
+
+                activity.startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                log(ERROR) { "Can't launch settings intent for Google Play: $e" }
+                Toast.makeText(activity, "Google Play is not installed", Toast.LENGTH_SHORT).show()
+            }
+        }
     )
 
+    companion object {
+        private const val GPLAY_PKG = "com.android.vending"
+    }
 }
