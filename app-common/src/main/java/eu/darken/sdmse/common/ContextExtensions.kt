00fc325929129de4b2ee076e37eeaa3198dbc5cf
darken <darken@darken.eu>
2023-12-07 12:59:30 +0100

    AppCleaner: Improve LineageOS detection

--- app-common/src/main/java/eu/darken/sdmse/common/ContextExtensions.kt
+++ app-common/src/main/java/eu/darken/sdmse/common/ContextExtensions.kt
@@ -3,6 +3,7 @@ package eu.darken.sdmse.common
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.ColorStateList
 import android.content.res.TypedArray
 import android.util.TypedValue
@@ -65,4 +66,11 @@ fun Context.openAsset(path: String): Source {
 
 fun Context.getColorStateListFor(@ColorRes colorRes: Int): ColorStateList {
     return ColorStateList.valueOf(ContextCompat.getColor(this, colorRes))
+}
+
+fun Context.isInstalled(pkgName: String) = try {
+    @Suppress("DEPRECATION")
+    this.packageManager.getPackageInfo(pkgName, 0) != null
+} catch (_: PackageManager.NameNotFoundException) {
+    false
 }
