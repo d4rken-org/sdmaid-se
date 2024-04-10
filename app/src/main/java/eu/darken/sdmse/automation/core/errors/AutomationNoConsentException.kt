fcd86b266c9b6ba7593420e1a5b1e0b2b4f0c1cb
darken <darken@darken.eu>
2024-04-10 19:18:06 +0200

    Automation: Allow going directly to setup from the ACS unavailable error dialog

--- app/src/main/java/eu/darken/sdmse/automation/core/errors/AutomationNoConsentException.kt
+++ app/src/main/java/eu/darken/sdmse/automation/core/errors/AutomationNoConsentException.kt
@@ -1,9 +1,13 @@
 package eu.darken.sdmse.automation.core.errors
 
+import androidx.navigation.Navigation
+import eu.darken.sdmse.MainDirections
 import eu.darken.sdmse.R
 import eu.darken.sdmse.common.ca.toCaString
 import eu.darken.sdmse.common.error.HasLocalizedError
 import eu.darken.sdmse.common.error.LocalizedError
+import eu.darken.sdmse.setup.SetupModule
+import eu.darken.sdmse.setup.SetupScreenOptions
 
 open class AutomationNoConsentException(
     override val message: String = "User has not consented to accessibility service being used"
@@ -13,6 +17,12 @@ open class AutomationNoConsentException(
         throwable = this,
         label = R.string.automation_error_no_consent_title.toCaString(),
         description = R.string.automation_error_no_consent_body.toCaString(),
+        fixActionLabel = R.string.setup_title.toCaString(),
+        fixAction = {
+            val navController = Navigation.findNavController(it, R.id.nav_host)
+            val options = SetupScreenOptions(showCompleted = true, typeFilter = setOf(SetupModule.Type.AUTOMATION))
+            navController.navigate(MainDirections.goToSetup(options = options))
+        }
     )
 
 }
