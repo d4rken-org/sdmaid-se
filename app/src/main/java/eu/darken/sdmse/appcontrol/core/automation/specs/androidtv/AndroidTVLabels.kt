af9f9ea7fa59099c19bf2a3d1fc7bcb4df926bad
darken <darken@darken.eu>
2024-04-10 19:11:40 +0200

    Support force-stopping via ACS on Android TV

--- app/src/main/java/eu/darken/sdmse/appcontrol/core/automation/specs/androidtv/AndroidTVLabels.kt
+++ app/src/main/java/eu/darken/sdmse/appcontrol/core/automation/specs/androidtv/AndroidTVLabels.kt
@@ -1,13 +1,34 @@
 package eu.darken.sdmse.appcontrol.core.automation.specs.androidtv
 
+import android.content.Context
 import dagger.Reusable
+import dagger.hilt.android.qualifiers.ApplicationContext
 import eu.darken.sdmse.appcontrol.core.automation.specs.AppControlLabelSource
 import eu.darken.sdmse.common.debug.logging.logTag
 import javax.inject.Inject
 
 @Reusable
 open class AndroidTVLabels @Inject constructor(
+    @ApplicationContext private val context: Context,
 ) : AppControlLabelSource {
+
+    fun getForceStopButtonDynamic(): Set<String> = setOf(
+        "device_apps_app_management_force_stop",
+    ).getAsStringResources(context, AndroidTVSpecs.SETTINGS_PKG)
+
+    fun getForceStopDialogText(): Set<String> = setOf(
+        "device_apps_app_management_force_stop_desc",
+    ).getAsStringResources(context, AndroidTVSpecs.SETTINGS_PKG)
+
+    fun getForceStopDialogOkDynamic(): Set<String> = setOf(
+        "okay",
+        "dlg_ok",
+    ).getAsStringResources(context, AndroidTVSpecs.SETTINGS_PKG)
+
+    fun getForceStopDialogCancelDynamic(): Set<String> = setOf(
+        "cancel",
+        "dlg_cancel",
+    ).getAsStringResources(context, AndroidTVSpecs.SETTINGS_PKG)
 
     companion object {
         val TAG: String = logTag("AppControl", "Automation", "AndroidTV", "Specs")
--- app/src/main/java/eu/darken/sdmse/appcontrol/core/automation/specs/androidtv/AndroidTVSpecs.kt
+++ app/src/main/java/eu/darken/sdmse/appcontrol/core/automation/specs/androidtv/AndroidTVSpecs.kt
@@ -1,6 +1,7 @@
 package eu.darken.sdmse.appcontrol.core.automation.specs.androidtv
 
 import android.content.Context
+import android.view.accessibility.AccessibilityNodeInfo
 import dagger.Binds
 import dagger.Module
 import dagger.Reusable
@@ -9,10 +10,21 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import dagger.multibindings.IntoSet
 import eu.darken.sdmse.appcontrol.core.automation.specs.AppControlSpecGenerator
+import eu.darken.sdmse.automation.core.common.StepProcessor
+import eu.darken.sdmse.automation.core.common.clickableParent
+import eu.darken.sdmse.automation.core.common.crawl
+import eu.darken.sdmse.automation.core.common.defaultClick
+import eu.darken.sdmse.automation.core.common.defaultWindowFilter
+import eu.darken.sdmse.automation.core.common.defaultWindowIntent
+import eu.darken.sdmse.automation.core.common.getDefaultNodeRecovery
 import eu.darken.sdmse.automation.core.common.getSysLocale
+import eu.darken.sdmse.automation.core.common.pkgId
+import eu.darken.sdmse.automation.core.common.textMatchesAny
+import eu.darken.sdmse.automation.core.common.windowCriteriaAppIdentifier
 import eu.darken.sdmse.automation.core.specs.AutomationExplorer
 import eu.darken.sdmse.automation.core.specs.AutomationSpec
 import eu.darken.sdmse.common.datastore.value
+import eu.darken.sdmse.common.debug.Bugs
 import eu.darken.sdmse.common.debug.logging.Logging.Priority.INFO
 import eu.darken.sdmse.common.debug.logging.Logging.Priority.VERBOSE
 import eu.darken.sdmse.common.debug.logging.log
@@ -22,6 +34,7 @@ import eu.darken.sdmse.common.device.RomType
 import eu.darken.sdmse.common.funnel.IPCFunnel
 import eu.darken.sdmse.common.pkgs.features.Installed
 import eu.darken.sdmse.common.pkgs.toPkgId
+import eu.darken.sdmse.common.progress.withProgress
 import eu.darken.sdmse.main.core.GeneralSettings
 import java.util.*
 import javax.inject.Inject
@@ -31,7 +44,7 @@ open class AndroidTVSpecs @Inject constructor(
     private val ipcFunnel: IPCFunnel,
     @ApplicationContext private val context: Context,
     private val deviceDetective: DeviceDetective,
-    private val androidTVLabels: AndroidTVLabels,
+    private val tvLabels: AndroidTVLabels,
     private val generalSettings: GeneralSettings,
 ) : AppControlSpecGenerator {
 
@@ -51,7 +64,7 @@ open class AndroidTVSpecs @Inject constructor(
         }
     }
 
-    private val mainPlan: suspend AutomationExplorer.Context.(Installed) -> Unit = { pkg ->
+    private val mainPlan: suspend AutomationExplorer.Context.(Installed) -> Unit = plan@{ pkg ->
         log(TAG, INFO) { "Executing plan for ${pkg.installId} with context $this" }
 
         val locale = getSysLocale()
@@ -59,6 +72,62 @@ open class AndroidTVSpecs @Inject constructor(
         val script = locale.script
 
         log(VERBOSE) { "Getting specs for ${pkg.packageName} (lang=$lang, script=$script)" }
+
+        val forceStopLabels = tvLabels.getForceStopButtonDynamic()
+        var wasDisabled = false
+
+        run {
+            val step = StepProcessor.Step(
+                parentTag = tag,
+                label = "Find & click 'Force Stop' (targets=$forceStopLabels)",
+                windowIntent = defaultWindowIntent(pkg),
+                windowEventFilter = defaultWindowFilter(SETTINGS_PKG),
+                windowNodeTest = windowCriteriaAppIdentifier(SETTINGS_PKG, ipcFunnel, pkg),
+                nodeTest = storageFilter@{ node ->
+                    node.textMatchesAny(forceStopLabels)
+                },
+                nodeRecovery = getDefaultNodeRecovery(pkg),
+                nodeMapping = clickableParent(),
+                action = defaultClick(onDisabled = {
+                    wasDisabled = true
+                    true
+                }),
+            )
+            stepper.withProgress(this) { process(step) }
+        }
+
+        if (wasDisabled) {
+            log(TAG) { "Force stop button was disabled, app is already stopped." }
+            return@plan
+        }
+
+        run {
+            val dialogTxt = tvLabels.getForceStopDialogText()
+            val okLbl = tvLabels.getForceStopDialogOkDynamic()
+            val cancelLbl = tvLabels.getForceStopDialogCancelDynamic()
+
+            val windowCriteria = fun(node: AccessibilityNodeInfo): Boolean {
+                if (node.pkgId != SETTINGS_PKG) return false
+                return node.crawl().map { it.node }.any { subNode ->
+                    return@any subNode.viewIdResourceName?.contains("guidance_container") == true
+                }
+            }
+
+            val buttonFilter = fun(node: AccessibilityNodeInfo): Boolean = when (Bugs.isDryRun) {
+                true -> node.textMatchesAny(cancelLbl)
+                false -> node.textMatchesAny(okLbl)
+            }
+
+            val step = StepProcessor.Step(
+                parentTag = TAG,
+                label = "Find & click 'OK' in confirmation dialog ($okLbl,$cancelLbl)",
+                windowNodeTest = windowCriteria,
+                nodeTest = buttonFilter,
+                nodeMapping = clickableParent(),
+                action = defaultClick(),
+            )
+            stepper.withProgress(this) { process(step) }
+        }
     }
 
     @Module @InstallIn(SingletonComponent::class)
@@ -67,8 +136,7 @@ open class AndroidTVSpecs @Inject constructor(
     }
 
     companion object {
-        val TAG: String = logTag("AppControl", "Automation", "AndroidTV", "Specs")
         val SETTINGS_PKG = "com.android.tv.settings".toPkgId()
-
+        val TAG: String = logTag("AppControl", "Automation", "AndroidTV", "Specs")
     }
 }
