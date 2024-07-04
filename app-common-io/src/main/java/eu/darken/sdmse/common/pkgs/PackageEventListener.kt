96fb5223702f13bfd2df989f8f687548193796c6
darken <darken@darken.eu>
2024-07-04 14:04:29 +0200

    Pkg-IO: Fix ANR if package event receiver blocks during event emission

--- app-common-io/src/main/java/eu/darken/sdmse/common/pkgs/PackageEventListener.kt
+++ app-common-io/src/main/java/eu/darken/sdmse/common/pkgs/PackageEventListener.kt
@@ -8,15 +8,17 @@ import androidx.core.content.ContextCompat
 import dagger.hilt.android.qualifiers.ApplicationContext
 import eu.darken.sdmse.common.coroutine.AppScope
 import eu.darken.sdmse.common.debug.logging.Logging.Priority.ERROR
+import eu.darken.sdmse.common.debug.logging.asLog
 import eu.darken.sdmse.common.debug.logging.log
+import eu.darken.sdmse.common.debug.logging.logTag
 import eu.darken.sdmse.common.flow.setupCommonEventHandlers
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -32,25 +34,36 @@ class PackageEventListener @Inject constructor(
     }
 
     val events: Flow<Event> = callbackFlow {
-        val receiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                log { "onReceive(context=$context, intent=$intent)" }
+        fun sendAsync(intent: Intent) {
+            launch {
+                try {
+                    val event = when (intent.action) {
+                        Intent.ACTION_PACKAGE_ADDED -> {
+                            val pkgId = intent.data?.encodedSchemeSpecificPart?.toPkgId()
+                                ?: throw IllegalArgumentException("Package Info is missing in ${intent.data}")
+                            Event.PackageInstalled(pkgId)
+                        }
 
-                when (intent.action) {
-                    Intent.ACTION_PACKAGE_ADDED -> {
-                        val pkgId = intent.data?.encodedSchemeSpecificPart?.toPkgId()
-                        pkgId?.let { trySendBlocking(Event.PackageInstalled(it)) }
+                        Intent.ACTION_PACKAGE_REMOVED -> {
+                            val pkgId = intent.data?.encodedSchemeSpecificPart?.toPkgId()
+                                ?: throw IllegalArgumentException("Package Info is missing in ${intent.data}")
+                            Event.PackageRemoved(pkgId)
+                        }
+
+                        else -> throw IllegalArgumentException("Unknown intent action: ${intent.action}")
                     }
-
-                    Intent.ACTION_PACKAGE_REMOVED -> {
-                        val pkgId = intent.data?.encodedSchemeSpecificPart?.toPkgId()
-                        pkgId?.let { trySendBlocking(Event.PackageRemoved(it)) }
-                    }
-
-                    else -> log(ERROR) { "Unknown intent: $intent" }
+                    send(event)
+                } catch (e: Exception) {
+                    log(TAG, ERROR) { "Sending event failed: ${e.asLog()}" }
                 }
             }
+        }
 
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                log(TAG) { "onReceive(context=$context, intent=$intent)" }
+                sendAsync(intent)
+            }
         }
 
         val intentFilter = IntentFilter().apply {
@@ -62,11 +75,15 @@ class PackageEventListener @Inject constructor(
         ContextCompat.registerReceiver(context, receiver, intentFilter, ContextCompat.RECEIVER_NOT_EXPORTED)
 
         awaitClose {
-            log { "unregisterReceiver($receiver)" }
+            log(TAG) { "unregisterReceiver($receiver)" }
             context.unregisterReceiver(receiver)
         }
     }
         .shareIn(appScope, SharingStarted.WhileSubscribed(stopTimeoutMillis = 3000), 0)
-        .setupCommonEventHandlers("AppEventListener") { "events" }
+        .setupCommonEventHandlers(TAG) { "events" }
 
+
+    companion object {
+        private val TAG = logTag("Pkg", "EventListener")
+    }
 }
