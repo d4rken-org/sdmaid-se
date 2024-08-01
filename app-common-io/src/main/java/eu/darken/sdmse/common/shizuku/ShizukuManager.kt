0cbe889fce6f2a2c2a283d346ca332af49d4295a
darken <darken@darken.eu>
2024-08-01 17:42:59 +0200

    Root/Shizuku: Don't release availability state immediately
    During cold launches these states may repeatedly be checked, if we release immediately we do unnecessary work

--- app-common-io/src/main/java/eu/darken/sdmse/common/shizuku/ShizukuManager.kt
+++ app-common-io/src/main/java/eu/darken/sdmse/common/shizuku/ShizukuManager.kt
@@ -12,19 +12,21 @@ import eu.darken.sdmse.common.debug.logging.log
 import eu.darken.sdmse.common.debug.logging.logTag
 import eu.darken.sdmse.common.flow.replayingShare
 import eu.darken.sdmse.common.flow.setupCommonEventHandlers
-import eu.darken.sdmse.common.flow.shareLatest
 import eu.darken.sdmse.common.pkgs.Pkg
 import eu.darken.sdmse.common.pkgs.toPkgId
 import eu.darken.sdmse.common.shizuku.service.ShizukuServiceClient
 import eu.darken.sdmse.common.shizuku.service.internal.ShizukuBaseServiceBinder
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
@@ -153,7 +155,15 @@ class ShizukuManager @Inject constructor(
                 permissionGrantEvents.map { }.onStart { emit(Unit) },
             ) { _, _ -> isShizukud() }
         }
-        .shareLatest(appScope)
+        .stateIn(
+            scope = appScope,
+            started = SharingStarted.WhileSubscribed(
+                stopTimeoutMillis = 3000,
+                replayExpirationMillis = 0,
+            ),
+            initialValue = null
+        )
+        .filterNotNull()
 
     companion object {
         private val TAG = logTag("Shizuku", "Manager")
