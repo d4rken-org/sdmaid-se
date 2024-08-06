c1603eaa4e55e9a37d356a074f8239b04bc150ab
darken <darken@darken.eu>
2024-08-06 20:50:27 +0200

    Analyzer: Better error handling if content can't opened

--- app/src/main/java/eu/darken/sdmse/analyzer/ui/storage/content/ContentItemEvents.kt
+++ app/src/main/java/eu/darken/sdmse/analyzer/ui/storage/content/ContentItemEvents.kt
@@ -1,9 +1,11 @@
 package eu.darken.sdmse.analyzer.ui.storage.content
 
+import android.content.Intent
 import eu.darken.sdmse.analyzer.core.content.ContentItem
 
 sealed class ContentItemEvents {
     data class ShowNoAccessHint(val item: ContentItem) : ContentItemEvents()
     data class ExclusionsCreated(val count: Int) : ContentItemEvents()
     data class ContentDeleted(val count: Int, val freedSpace: Long) : ContentItemEvents()
+    data class OpenContent(val intent: Intent) : ContentItemEvents()
 }
