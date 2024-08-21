489e3121f5f0ef353e3af5fe0c44f7ad12604790
darken <darken@darken.eu>
2024-08-21 14:09:48 +0200

    AppCleaner: Fix duplicate paths in default caches filter

--- app/src/main/java/eu/darken/sdmse/appcleaner/core/AppCleanerExtensions.kt
+++ app/src/main/java/eu/darken/sdmse/appcleaner/core/AppCleanerExtensions.kt
@@ -1,12 +1,13 @@
 package eu.darken.sdmse.appcleaner.core
 
 import eu.darken.sdmse.appcleaner.core.forensics.ExpendablesFilter
+import eu.darken.sdmse.appcleaner.core.forensics.ExpendablesFilterIdentifier
 import eu.darken.sdmse.exclusion.core.types.Exclusion
 import eu.darken.sdmse.exclusion.core.types.excludeNestedLookups
+import kotlin.reflect.KClass
 
 val AppCleaner.Data?.hasData: Boolean
     get() = this?.junks?.isNotEmpty() ?: false
-
 
 suspend fun Collection<Exclusion.Path>.excludeNestedLookups(
     matches: Collection<ExpendablesFilter.Match>
@@ -17,3 +18,6 @@ suspend fun Collection<Exclusion.Path>.excludeNestedLookups(
         .filter { temp.contains(it.lookup) }
         .toSet()
 }
+
+val KClass<out ExpendablesFilter>.identifier: ExpendablesFilterIdentifier
+    get() = this
