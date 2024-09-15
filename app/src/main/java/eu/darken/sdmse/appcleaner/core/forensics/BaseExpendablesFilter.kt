a0ea4e4cf0e755488c89c5c3b9cdffad424a715b
darken <darken@darken.eu>
2024-09-15 16:18:45 +0200

    AppCleaner: Make deletion more resilient
    If deletion fails, check if the file actually exists, if it doesn't, treat it as success.
    
    Closes #1391

--- app/src/main/java/eu/darken/sdmse/appcleaner/core/forensics/BaseExpendablesFilter.kt
+++ app/src/main/java/eu/darken/sdmse/appcleaner/core/forensics/BaseExpendablesFilter.kt
@@ -2,6 +2,7 @@ package eu.darken.sdmse.appcleaner.core.forensics
 
 import eu.darken.sdmse.common.debug.Bugs
 import eu.darken.sdmse.common.debug.logging.Logging.Priority.ERROR
+import eu.darken.sdmse.common.debug.logging.Logging.Priority.VERBOSE
 import eu.darken.sdmse.common.debug.logging.Logging.Priority.WARN
 import eu.darken.sdmse.common.debug.logging.log
 import eu.darken.sdmse.common.debug.logging.logTag
@@ -9,7 +10,9 @@ import eu.darken.sdmse.common.files.APathLookup
 import eu.darken.sdmse.common.files.GatewaySwitch
 import eu.darken.sdmse.common.files.PathException
 import eu.darken.sdmse.common.files.deleteAll
+import eu.darken.sdmse.common.files.exists
 import eu.darken.sdmse.common.files.filterDistinctRoots
+import eu.darken.sdmse.common.files.isAncestorOf
 import eu.darken.sdmse.common.flow.throttleLatest
 import eu.darken.sdmse.common.progress.Progress
 import eu.darken.sdmse.common.progress.updateProgressSecondary
@@ -30,7 +33,7 @@ abstract class BaseExpendablesFilter : ExpendablesFilter {
     ): ExpendablesFilter.ProcessResult = this
         .map { it as ExpendablesFilter.Match.Deletion }
         .let { deletionMatches ->
-            val success = mutableSetOf<ExpendablesFilter.Match.Deletion>()
+            val successful = mutableSetOf<ExpendablesFilter.Match.Deletion>()
             val failed = mutableSetOf<Pair<ExpendablesFilter.Match.Deletion, Exception>>()
 
             val distinctRoots = deletionMatches.map { it.lookup }.filterDistinctRoots()
@@ -44,26 +47,63 @@ abstract class BaseExpendablesFilter : ExpendablesFilter {
                 }
             }
 
-            distinctRoots.forEach { targetContent ->
-                updateProgressSecondary(targetContent.userReadablePath)
-                val originalmatch = deletionMatches.first { it.lookup == targetContent }
-                try {
-                    targetContent.deleteAll(gatewaySwitch)
-                    success.add(originalmatch)
-                } catch (e: PathException) {
-                    log(logTag("AppCleaner,BaseExpendablesFilter"), ERROR) {
-                        "Failed to delete $originalmatch due to $e"
+            val workingSet = deletionMatches.toMutableSet()
+
+            distinctRoots.forEach { targetRoot ->
+                updateProgressSecondary(targetRoot.userReadablePath)
+                val mainmatch = workingSet.first { it.lookup == targetRoot }
+                val alsoAffected = workingSet
+                    .filter { it != mainmatch && mainmatch.lookup.isAncestorOf(it.lookup) }
+                    .toSet()
+
+                val mainDeleted = try {
+                    targetRoot.deleteAll(gatewaySwitch)
+                    log(TAG, VERBOSE) { "Main match deleted" }
+                    true
+                } catch (oge: PathException) {
+                    try {
+                        if (targetRoot.exists(gatewaySwitch)) {
+                            log(TAG, WARN) { "Deletion failed, file still exists" }
+                            failed.add(mainmatch to oge)
+                            false
+                        } else {
+                            log(TAG, WARN) { "Deletion failed as file no longer exists, okay..." }
+                            true
+                        }
+                    } catch (e: PathException) {
+                        log(TAG, ERROR) { "Post-deletion-failure-exist check failed too on $mainmatch due to $e" }
+                        failed.add(mainmatch to e)
+                        false
                     }
-                    failed.add(originalmatch to e)
+                }
+
+                if (mainDeleted) {
+                    workingSet.remove(mainmatch)
+                    successful.add(mainmatch)
+                    if (Bugs.isDebug) {
+                        log(TAG, VERBOSE) { "Main deleted:\n$mainmatch" }
+                    }
+                }
+
+                if (mainDeleted) {
+                    workingSet.removeAll(alsoAffected)
+                    successful.addAll(alsoAffected)
+                    if (Bugs.isDebug) {
+                        log(TAG, VERBOSE) { "Also deleted:\n${alsoAffected.joinToString("\n")}" }
+                    }
                 }
             }
 
             ExpendablesFilter.ProcessResult(
-                success = success,
+                success = successful,
                 failed = failed,
             )
         }
 
     suspend fun APathLookup<*>.toDeletionMatch() = ExpendablesFilter.Match.Deletion(identifier, this)
 
+
+    companion object {
+        private val TAG = logTag("AppCleaner", "BaseExpendablesFilter")
+    }
 }
