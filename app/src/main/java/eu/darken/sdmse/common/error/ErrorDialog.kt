42ba654cf419424a4a87f8b5619ee244851ef8a7
darken <darken@darken.eu>
2024-09-24 21:09:40 +0200

    Make text in error dialogs selectable/copyable

--- app/src/main/java/eu/darken/sdmse/common/error/ErrorDialog.kt
+++ app/src/main/java/eu/darken/sdmse/common/error/ErrorDialog.kt
@@ -1,29 +1,49 @@
 package eu.darken.sdmse.common.error
 
 import android.app.Activity
+import android.util.TypedValue
 import androidx.navigation.findNavController
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textview.MaterialTextView
 import eu.darken.sdmse.common.R
 import eu.darken.sdmse.common.files.WriteException
 import eu.darken.sdmse.exclusion.ui.editor.path.PathExclusionEditorOptions
 import eu.darken.sdmse.exclusion.ui.editor.path.PathExclusionFragmentArgs
 
 fun Throwable.asErrorDialogBuilder(
-    activity: Activity
+    context: Activity
 ): MaterialAlertDialogBuilder {
-    val context = activity
     return MaterialAlertDialogBuilder(context).apply {
         val error = this@asErrorDialogBuilder
         val localizedError = error.localized(context)
 
         setTitle(localizedError.label.get(context))
-        setMessage(localizedError.description.get(context))
+
+        val messageView = MaterialTextView(context).apply {
+            text = localizedError.description.get(context)
+            setTextAppearance(com.google.android.material.R.style.TextAppearance_MaterialComponents_Caption)
+            setTextIsSelectable(true)
+
+            val paddingHorizontal = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 24f, resources.displayMetrics
+            ).toInt()
+            val paddingVertical = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 8f, resources.displayMetrics
+            ).toInt()
+            setPadding(
+                paddingHorizontal,
+                paddingVertical,
+                paddingHorizontal,
+                0,
+            )
+        }
+        setView(messageView)
 
         if (localizedError.fixAction != null) {
             setPositiveButton(
                 localizedError.fixActionLabel?.get(context) ?: context.getString(android.R.string.ok)
             ) { _, _ ->
-                localizedError.fixAction!!.invoke(activity)
+                localizedError.fixAction!!.invoke(context)
             }
             setNegativeButton(R.string.general_cancel_action) { _, _ ->
             }
@@ -35,13 +55,13 @@ fun Throwable.asErrorDialogBuilder(
         when {
             localizedError.infoAction != null -> {
                 setNeutralButton(R.string.general_show_details_action) { _, _ ->
-                    localizedError.infoAction!!.invoke(activity)
+                    localizedError.infoAction!!.invoke(context)
                 }
             }
 
             this@asErrorDialogBuilder is WriteException && path != null -> {
                 setNeutralButton(eu.darken.sdmse.R.string.exclusion_create_action) { _, _ ->
-                    activity.findNavController(eu.darken.sdmse.R.id.nav_host).navigate(
+                    context.findNavController(eu.darken.sdmse.R.id.nav_host).navigate(
                         resId = eu.darken.sdmse.R.id.goToPathExclusionEditor,
                         args = PathExclusionFragmentArgs(
                             initial = PathExclusionEditorOptions(targetPath = path!!)
