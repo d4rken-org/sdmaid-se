a93145d637abce6011c0cfff0a302ff810edf231
darken <darken@darken.eu>
2024-12-22 16:07:37 +0100

    Reduce duplicate strings

--- app/src/main/java/eu/darken/sdmse/analyzer/core/content/ContentDeleteTask.kt
+++ app/src/main/java/eu/darken/sdmse/analyzer/core/content/ContentDeleteTask.kt
@@ -35,7 +35,7 @@ data class ContentDeleteTask(
                 val spaceText = run {
                     val (spaceFormatted, spaceQuantity) = ByteFormatter.formatSize(this, affectedSpace)
                     getQuantityString2(
-                        eu.darken.sdmse.common.R.plurals.general_delete_success_freed_y,
+                        eu.darken.sdmse.common.R.plurals.general_result_x_space_freed,
                         spaceQuantity,
                         spaceFormatted,
                     )
