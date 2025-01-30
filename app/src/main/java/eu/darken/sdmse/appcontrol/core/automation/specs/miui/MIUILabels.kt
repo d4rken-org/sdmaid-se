e6a26c146b3137dbd82c189f01bafad98eb81d88
darken <darken@darken.eu>
2025-01-30 18:07:54 +0100

    Fix label lookup
    On my test device the MIUI security center didn't contain any relevant strings under that ID

--- app/src/main/java/eu/darken/sdmse/appcontrol/core/automation/specs/miui/MIUILabels.kt
+++ app/src/main/java/eu/darken/sdmse/appcontrol/core/automation/specs/miui/MIUILabels.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import dagger.Reusable
 import dagger.hilt.android.qualifiers.ApplicationContext
 import eu.darken.sdmse.appcontrol.core.automation.specs.AppControlLabelSource
+import eu.darken.sdmse.appcontrol.core.automation.specs.aosp.AOSPLabels
+import eu.darken.sdmse.appcontrol.core.automation.specs.hyperos.HyperOsLabels
 import eu.darken.sdmse.common.debug.logging.logTag
 import eu.darken.sdmse.common.pkgs.toPkgId
 import javax.inject.Inject
@@ -11,25 +13,53 @@ import javax.inject.Inject
 @Reusable
 class MIUILabels @Inject constructor(
     @ApplicationContext private val context: Context,
+    private val aospLabels: AOSPLabels,
 ) : AppControlLabelSource {
 
-    fun getForceStopButtonDynamic(): Set<String> = setOf(
-        "force_stop",
-    ).getAsStringResources(context, SETTINGS_PKG)
+    fun getForceStopButtonDynamic(): Set<String> {
+        val labels = mutableSetOf<String>()
+        setOf("force_stop")
+            .getAsStringResources(context, HyperOsLabels.SETTINGS_PKG)
+            .run { labels.addAll(this) }
+        if (labels.isEmpty()) {
+            labels.addAll(aospLabels.getForceStopButtonDynamic())
+        }
+        return labels
+    }
 
-    fun getForceStopDialogTitleDynamic(): Set<String> = setOf(
-        "force_stop_dlg_title",
-    ).getAsStringResources(context, SETTINGS_PKG)
+    fun getForceStopDialogTitleDynamic(): Set<String> {
+        val labels = mutableSetOf<String>()
+        setOf("force_stop_dlg_title")
+            .getAsStringResources(context, HyperOsLabels.SETTINGS_PKG)
+            .run { labels.addAll(this) }
+        if (labels.isEmpty()) {
+            labels.addAll(aospLabels.getForceStopDialogTitleDynamic())
+        }
+        return labels
+    }
 
-    fun getForceStopDialogOkDynamic(): Set<String> = setOf(
-        "okay",
-        "dlg_ok",
-    ).getAsStringResources(context, SETTINGS_PKG)
+    fun getForceStopDialogOkDynamic(): Set<String> {
+        val labels = mutableSetOf<String>()
+        setOf("okay", "dlg_ok")
+            .getAsStringResources(context, HyperOsLabels.SETTINGS_PKG)
+            .run { labels.addAll(this) }
+        if (labels.isEmpty()) {
+            labels.addAll(aospLabels.getForceStopDialogOkDynamic())
+        }
+        return labels
+    }
 
-    fun getForceStopDialogCancelDynamic(): Set<String> = setOf(
-        "cancel",
-        "dlg_cancel",
-    ).getAsStringResources(context, SETTINGS_PKG)
+    fun getForceStopDialogCancelDynamic(): Set<String> {
+        val labels = mutableSetOf<String>()
+        setOf("cancel", "dlg_cancel")
+            .getAsStringResources(context, HyperOsLabels.SETTINGS_PKG)
+            .run { labels.addAll(this) }
+        if (labels.isEmpty()) {
+            labels.addAll(aospLabels.getForceStopDialogOkDynamic())
+        }
+        return labels
+    }
+
 
     companion object {
         val SETTINGS_PKG = "com.miui.securitycenter".toPkgId()
