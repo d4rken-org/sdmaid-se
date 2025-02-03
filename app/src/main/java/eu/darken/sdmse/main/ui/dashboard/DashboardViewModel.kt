ce0cb52ad64c0ea374b100a6d7c74404c8aa1353
darken <darken@darken.eu>
2025-02-03 18:37:52 +0100

    Remove debug output on App INIT timing that is no longer needed

--- app/src/main/java/eu/darken/sdmse/main/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/eu/darken/sdmse/main/ui/dashboard/DashboardViewModel.kt
@@ -2,7 +2,6 @@ package eu.darken.sdmse.main.ui.dashboard
 
 import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
-import eu.darken.sdmse.App
 import eu.darken.sdmse.MainDirections
 import eu.darken.sdmse.analyzer.core.Analyzer
 import eu.darken.sdmse.analyzer.ui.AnalyzerDashCardVH
@@ -22,7 +21,6 @@ import eu.darken.sdmse.common.areas.DataAreaManager
 import eu.darken.sdmse.common.coroutine.DispatcherProvider
 import eu.darken.sdmse.common.datastore.value
 import eu.darken.sdmse.common.datastore.valueBlocking
-import eu.darken.sdmse.common.debug.Bugs
 import eu.darken.sdmse.common.debug.DebugCardProvider
 import eu.darken.sdmse.common.debug.logging.Logging.Priority.INFO
 import eu.darken.sdmse.common.debug.logging.Logging.Priority.VERBOSE
@@ -90,7 +88,6 @@ import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapLatest
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import java.time.Duration
 import java.time.Instant
@@ -550,11 +547,6 @@ class DashboardViewModel @Inject constructor(
             isEasterEgg = easterEggTriggered,
         )
     }
-        .onEach {
-            if (!Bugs.isDebug) return@onEach
-            val stop = System.currentTimeMillis()
-            log(TAG, VERBOSE) { "Time to dashboard (TTD) ${stop - App.INIT_AT}ms" }
-        }
         .throttleLatest(500)
         .replayingShare(vmScope)
 
