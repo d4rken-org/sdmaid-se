e3f70d25ade0eced8e5a74f6b086d820c13c84da
darken <darken@darken.eu>
2025-03-19 22:45:32 +0100

    Update privacy policy layout
    Switching to GitHub pages to deal with Google no longer allowing direct links to GitHub.

--- app-common/src/main/java/eu/darken/sdmse/common/SdmSeLinks.kt
+++ app-common/src/main/java/eu/darken/sdmse/common/SdmSeLinks.kt
@@ -2,5 +2,5 @@ package eu.darken.sdmse.common
 
 object SdmSeLinks {
     const val ISSUES = "https://github.com/d4rken-org/sdmaid-se/issues"
-    const val PRIVACY_POLICY = "https://github.com/d4rken-org/sdmaid-se/blob/main/PRIVACY_POLICY.md"
+    const val PRIVACY_POLICY = "https://sdmse.darken.eu/privacy"
 }
