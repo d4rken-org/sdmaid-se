8148ea221f5d9b7f9ab662d84542d22a0e3c9973
darken <darken@darken.eu>
2025-03-23 20:05:49 +0100

    Remove duplicate code and improve readability

--- app/src/main/java/eu/darken/sdmse/systemcleaner/core/sieve/BaseSieve.kt
+++ app/src/main/java/eu/darken/sdmse/systemcleaner/core/sieve/BaseSieve.kt
@@ -9,6 +9,7 @@ import eu.darken.sdmse.common.debug.logging.Logging.Priority.WARN
 import eu.darken.sdmse.common.debug.logging.log
 import eu.darken.sdmse.common.debug.logging.logTag
 import eu.darken.sdmse.common.files.APathLookup
+import eu.darken.sdmse.common.files.Segments
 import eu.darken.sdmse.common.files.containsSegments
 import eu.darken.sdmse.common.files.endsWith
 import eu.darken.sdmse.common.files.isAncestorOf
@@ -41,15 +42,13 @@ class BaseSieve @AssistedInject constructor(
 
     suspend fun match(subject: APathLookup<*>): Result {
         // Directory or file?
-        config.targetTypes
-            ?.takeIf { it.isNotEmpty() }
-            ?.let { types ->
-                if (subject.isFile && !types.contains(TargetType.FILE)) {
-                    return Result(subject, matches = false)
-                } else if (subject.isDirectory && !types.contains(TargetType.DIRECTORY)) {
-                    return Result(subject, matches = false)
-                }
+        config.targetTypes?.takeIf { it.isNotEmpty() }?.let { types ->
+            if (subject.isFile && !types.contains(TargetType.FILE)) {
+                return Result(subject, matches = false)
+            } else if (subject.isDirectory && !types.contains(TargetType.DIRECTORY)) {
+                return Result(subject, matches = false)
             }
+        }
 
         config.maximumSize?.let {
             // Is our subject too large?
@@ -73,117 +72,49 @@ class BaseSieve @AssistedInject constructor(
             }
         }
 
-        config.pathCriteria
-            ?.takeIf { it.isNotEmpty() }
-            ?.let { criteria ->
-                val hasMatch = criteria.any { crit ->
-                    when (crit.mode) {
-                        is SegmentCriterium.Mode.Ancestor -> crit.segments.isAncestorOf(
-                            subject.segments,
-                            ignoreCase = crit.mode.ignoreCase,
-                        )
+        config.pathCriteria?.takeIf { it.isNotEmpty() }?.let { criteria ->
+            val hasMatch = criteria.match(subject.segments)
+            if (!hasMatch) return Result(subject, matches = false)
+        }
 
-                        is SegmentCriterium.Mode.Start -> subject.segments.startsWith(
-                            crit.segments,
-                            ignoreCase = crit.mode.ignoreCase,
-                            allowPartial = crit.mode.allowPartial
-                        )
+        config.nameCriteria?.takeIf { it.isNotEmpty() }?.let { criteria ->
+            val hasMatch = criteria.any { crit ->
+                when (crit.mode) {
+                    is NameCriterium.Mode.Start -> subject.name.startsWith(
+                        crit.name,
+                        ignoreCase = crit.mode.ignoreCase
+                    )
 
-                        is SegmentCriterium.Mode.End -> subject.segments.endsWith(
-                            crit.segments,
-                            ignoreCase = crit.mode.ignoreCase,
-                            allowPartial = crit.mode.allowPartial
-                        )
+                    is NameCriterium.Mode.End -> subject.name.endsWith(
+                        crit.name,
+                        ignoreCase = crit.mode.ignoreCase
+                    )
 
-                        is SegmentCriterium.Mode.Contain -> subject.segments.containsSegments(
-                            crit.segments,
-                            allowPartial = crit.mode.allowPartial,
-                            ignoreCase = crit.mode.ignoreCase
-                        )
+                    is NameCriterium.Mode.Contain -> subject.name.contains(
+                        crit.name,
+                        ignoreCase = crit.mode.ignoreCase
+                    )
 
-                        is SegmentCriterium.Mode.Equal -> subject.segments.matches(
-                            crit.segments,
-                            ignoreCase = crit.mode.ignoreCase
-                        )
-                    }
+                    is NameCriterium.Mode.Equal -> subject.name.equals(
+                        crit.name,
+                        ignoreCase = crit.mode.ignoreCase
+                    )
+
                 }
-                if (!hasMatch) return Result(subject, matches = false)
             }
 
-        config.nameCriteria
-            ?.takeIf { it.isNotEmpty() }
-            ?.let { criteria ->
-                val hasMatch = criteria.any { crit ->
-                    when (crit.mode) {
-                        is NameCriterium.Mode.Start -> subject.name.startsWith(
-                            crit.name,
-                            ignoreCase = crit.mode.ignoreCase
-                        )
+            if (!hasMatch) return Result(subject, matches = false)
+        }
 
-                        is NameCriterium.Mode.End -> subject.name.endsWith(
-                            crit.name,
-                            ignoreCase = crit.mode.ignoreCase
-                        )
+        config.pathExclusions?.takeIf { it.isNotEmpty() }?.let { exclusions ->
+            // Check what the path should not contain
+            val isExcluded = exclusions.match(subject.segments)
+            if (isExcluded) return Result(subject, matches = false)
+        }
 
-                        is NameCriterium.Mode.Contain -> subject.name.contains(
-                            crit.name,
-                            ignoreCase = crit.mode.ignoreCase
-                        )
-
-                        is NameCriterium.Mode.Equal -> subject.name.equals(
-                            crit.name,
-                            ignoreCase = crit.mode.ignoreCase
-                        )
-
-                    }
-                }
-
-                if (!hasMatch) return Result(subject, matches = false)
-            }
-
-        config.pathExclusions
-            ?.takeIf { it.isNotEmpty() }
-            ?.let { exclusions ->
-                // Check what the path should not contain
-                val isExcluded = exclusions.any { crit ->
-                    when (crit.mode) {
-                        is SegmentCriterium.Mode.Ancestor -> crit.segments.isAncestorOf(
-                            subject.segments,
-                            ignoreCase = crit.mode.ignoreCase,
-                        )
-
-                        is SegmentCriterium.Mode.Start -> subject.segments.startsWith(
-                            crit.segments,
-                            ignoreCase = crit.mode.ignoreCase,
-                            allowPartial = crit.mode.allowPartial
-                        )
-
-                        is SegmentCriterium.Mode.End -> subject.segments.endsWith(
-                            crit.segments,
-                            ignoreCase = crit.mode.ignoreCase,
-                            allowPartial = crit.mode.allowPartial
-                        )
-
-                        is SegmentCriterium.Mode.Contain -> subject.segments.containsSegments(
-                            crit.segments,
-                            allowPartial = crit.mode.allowPartial,
-                            ignoreCase = crit.mode.ignoreCase
-                        )
-
-                        is SegmentCriterium.Mode.Equal -> subject.segments.matches(
-                            crit.segments,
-                            ignoreCase = crit.mode.ignoreCase
-                        )
-                    }
-                }
-                if (isExcluded) return Result(subject, matches = false)
-            }
-
-        config.pathRegexes
-            ?.takeIf { it.isNotEmpty() }
-            ?.let { regexes ->
-                if (regexes.none { it.matches(subject.path) }) return Result(subject, matches = false)
-            }
+        config.pathRegexes?.takeIf { it.isNotEmpty() }?.let { regexes ->
+            if (regexes.none { it.matches(subject.path) }) return Result(subject, matches = false)
+        }
 
         val areaInfo = fileForensics.identifyArea(subject)
         if (areaInfo == null) {
@@ -192,96 +123,61 @@ class BaseSieve @AssistedInject constructor(
         }
         val pfpSegments = areaInfo.prefixFreeSegments
 
-        config.areaTypes
-            ?.takeIf { it.isNotEmpty() }
-            ?.let { types ->
-                if (!types.contains(areaInfo.type)) {
-                    return Result(subject, matches = false)
-                }
-            }
+        config.areaTypes?.takeIf { it.isNotEmpty() }?.let { types ->
+            val matchesArea = types.contains(areaInfo.type)
+            if (!matchesArea) return Result(subject, matches = false)
+        }
 
         // Now working on the prefix-free-path!
         // Check pfp startsWith/ancestorOf
 
-        config.pfpExclusions
-            ?.takeIf { it.isNotEmpty() }
-            ?.let { criteria ->
-                val isExcluded = criteria.any { crit ->
-                    when (crit.mode) {
-                        is SegmentCriterium.Mode.Ancestor -> crit.segments.isAncestorOf(
-                            pfpSegments,
-                            ignoreCase = crit.mode.ignoreCase,
-                        )
+        config.pfpExclusions?.takeIf { it.isNotEmpty() }?.let { criteria ->
+            val isExcluded = criteria.match(pfpSegments)
+            if (isExcluded) return Result(subject, matches = false)
+        }
 
-                        is SegmentCriterium.Mode.Start -> pfpSegments.startsWith(
-                            crit.segments,
-                            ignoreCase = crit.mode.ignoreCase,
-                            allowPartial = crit.mode.allowPartial,
-                        )
-
-                        is SegmentCriterium.Mode.End -> pfpSegments.endsWith(
-                            crit.segments,
-                            ignoreCase = crit.mode.ignoreCase,
-                            allowPartial = crit.mode.allowPartial,
-                        )
-
-                        is SegmentCriterium.Mode.Contain -> pfpSegments.containsSegments(
-                            crit.segments,
-                            allowPartial = crit.mode.allowPartial,
-                            ignoreCase = crit.mode.ignoreCase
-                        )
-
-                        is SegmentCriterium.Mode.Equal -> pfpSegments.matches(
-                            crit.segments,
-                            ignoreCase = crit.mode.ignoreCase
-                        )
-                    }
-                }
-                if (isExcluded) return Result(subject, matches = false)
-            }
-
-        config.pfpCriteria
-            ?.takeIf { it.isNotEmpty() }
-            ?.let { criteria ->
-                val hasMatch = criteria.any { crit ->
-                    when (crit.mode) {
-                        is SegmentCriterium.Mode.Ancestor -> crit.segments.isAncestorOf(
-                            pfpSegments,
-                            ignoreCase = crit.mode.ignoreCase,
-                        )
-
-                        is SegmentCriterium.Mode.Start -> pfpSegments.startsWith(
-                            crit.segments,
-                            ignoreCase = crit.mode.ignoreCase,
-                            allowPartial = crit.mode.allowPartial,
-                        )
-
-                        is SegmentCriterium.Mode.End -> pfpSegments.endsWith(
-                            crit.segments,
-                            ignoreCase = crit.mode.ignoreCase,
-                            allowPartial = crit.mode.allowPartial,
-                        )
-
-                        is SegmentCriterium.Mode.Contain -> pfpSegments.containsSegments(
-                            crit.segments,
-                            allowPartial = crit.mode.allowPartial,
-                            ignoreCase = crit.mode.ignoreCase
-                        )
-
-                        is SegmentCriterium.Mode.Equal -> pfpSegments.matches(
-                            crit.segments,
-                            ignoreCase = crit.mode.ignoreCase
-                        )
-                    }
-                }
-                if (!hasMatch) return Result(subject, matches = false)
-            }
+        config.pfpCriteria?.takeIf { it.isNotEmpty() }?.let { criteria ->
+            val hasMatch = criteria.match(pfpSegments)
+            if (!hasMatch) return Result(subject, matches = false)
+        }
 
         return Result(
             subject,
             matches = true,
             areaInfo = areaInfo
         )
+    }
+
+    private fun Collection<SegmentCriterium>.match(target: Segments): Boolean = any { crit ->
+        when (crit.mode) {
+            is SegmentCriterium.Mode.Ancestor -> crit.segments.isAncestorOf(
+                target,
+                ignoreCase = crit.mode.ignoreCase,
+            )
+
+            is SegmentCriterium.Mode.Start -> target.startsWith(
+                crit.segments,
+                ignoreCase = crit.mode.ignoreCase,
+                allowPartial = crit.mode.allowPartial,
+            )
+
+            is SegmentCriterium.Mode.End -> target.endsWith(
+                crit.segments,
+                ignoreCase = crit.mode.ignoreCase,
+                allowPartial = crit.mode.allowPartial,
+            )
+
+            is SegmentCriterium.Mode.Contain -> target.containsSegments(
+                crit.segments,
+                allowPartial = crit.mode.allowPartial,
+                ignoreCase = crit.mode.ignoreCase
+            )
+
+            is SegmentCriterium.Mode.Equal -> target.matches(
+                crit.segments,
+                ignoreCase = crit.mode.ignoreCase
+            )
+        }
     }
 
     data class Config(
