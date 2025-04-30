6031942a61bb6511f9cf7afbab4bb3fa8ccbdb05
darken <darken@darken.eu>
2025-04-30 18:58:52 +0200

    AutomationCompatibilityException should include the current app version

--- app/src/main/java/eu/darken/sdmse/automation/core/errors/AutomationCompatibilityException.kt
+++ app/src/main/java/eu/darken/sdmse/automation/core/errors/AutomationCompatibilityException.kt
@@ -2,6 +2,7 @@ package eu.darken.sdmse.automation.core.errors
 
 import android.os.Build
 import eu.darken.sdmse.R
+import eu.darken.sdmse.common.BuildConfigWrap
 import eu.darken.sdmse.common.ca.caString
 import eu.darken.sdmse.common.ca.toCaString
 import eu.darken.sdmse.common.error.HasLocalizedError
@@ -20,7 +21,7 @@ open class AutomationCompatibilityException(
                 
                
                 ${getString(eu.darken.sdmse.common.R.string.general_information_for_the_developer)}:
-                
+                v${BuildConfigWrap.VERSION_NAME} (${BuildConfigWrap.VERSION_CODE}) ${BuildConfigWrap.FLAVOR} [${BuildConfigWrap.BUILD_TYPE}]
                 ${Build.FINGERPRINT}
             """.trimIndent()
         },
