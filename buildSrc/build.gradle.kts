432ccaa4e9bbada1631e90cadd830956edffc0f2
darken <darken@darken.eu>
2025-05-07 18:21:55 +0200

    Bump gradle and AGP

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    implementation("com.android.tools.build:gradle:8.7.3")
+    implementation("com.android.tools.build:gradle:8.9.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
     implementation("com.squareup:javapoet:1.13.0")
 }
