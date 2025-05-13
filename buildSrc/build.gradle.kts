38ff2969bca1b8ea6ee983efac6cfb4d2ced60eb
darken <darken@darken.eu>
2025-05-13 14:08:10 +0200

    Update dependency versions
    Updated the versions of several dependencies, including Dagger, AndroidX Navigation, KSP, and Android Gradle Plugin.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    implementation("com.android.tools.build:gradle:8.9.2")
+    implementation("com.android.tools.build:gradle:8.10.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
     implementation("com.squareup:javapoet:1.13.0")
 }
