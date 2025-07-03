34022f685c5b685b4be588ef800ef14d95304a3d
darken <darken@darken.eu>
2025-07-03 22:22:50 +0200

    Translations: Update Brazilian Portuguese translations
    This commit updates several strings in the Brazilian Portuguese translation file (`values-pt-rBR/strings.xml`).

--- fastlane/remove_unsupported_languages.sh
+++ fastlane/remove_unsupported_languages.sh
@@ -14,5 +14,6 @@ rm -rv ./metadata/android/ur-IN
 rm -rv ./metadata/android/zu
 rm -rv ./metadata/android/si-LK
 rm -rv ./metadata/android/pa-IN
+rm -rv ./metadata/android/kaa
 find ./metadata/android -empty -type d -delete
 exit 0
