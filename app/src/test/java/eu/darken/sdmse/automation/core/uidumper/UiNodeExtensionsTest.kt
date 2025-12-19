00e5cb735bb80ac56d3579bba6a0235410bd42c5
darken <darken@darken.eu>
2025-12-19 18:42:57 +0100

    Fix: Add missing TestApplication config to Robolectric test
    Add `application = TestApplication::class` to `@Config` annotation in
    UiNodeExtensionsTest to fix CI test failures. Without this config,
    Robolectric tests can crash the JVM test executor.

--- app/src/test/java/eu/darken/sdmse/automation/core/uidumper/UiNodeExtensionsTest.kt
+++ app/src/test/java/eu/darken/sdmse/automation/core/uidumper/UiNodeExtensionsTest.kt
@@ -8,9 +8,10 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import testhelpers.BaseTest
+import testhelpers.TestApplication
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [33])
+@Config(sdk = [33], application = TestApplication::class)
 class UiNodeExtensionsTest : BaseTest() {
 
     private fun createNode(
