e2a69286f5eae938c994749f9f0c1f45e678d2c5
darken <darken@darken.eu>
2026-02-02 09:32:03 +0100

    Swiper: Reorder dashboard card position
    Move Swiper to appear after Analyzer instead of after Scheduler
    in the default dashboard card order.

--- app/src/main/java/eu/darken/sdmse/main/core/DashboardCardType.kt
+++ app/src/main/java/eu/darken/sdmse/main/core/DashboardCardType.kt
@@ -16,7 +16,7 @@ enum class DashboardCardType(
     DEDUPLICATOR(R.string.deduplicator_tool_name, R.drawable.ic_content_duplicate_24),
     APPCONTROL(R.string.appcontrol_tool_name, R.drawable.ic_apps),
     ANALYZER(R.string.analyzer_tool_name, R.drawable.baseline_data_usage_24),
-    SCHEDULER(R.string.scheduler_label, R.drawable.ic_alarm_check_24),
     SWIPER(R.string.swiper_tool_name, R.drawable.ic_baseline_swipe_24),
+    SCHEDULER(R.string.scheduler_label, R.drawable.ic_alarm_check_24),
     STATS(R.string.stats_label, R.drawable.ic_chartbox_24),
 }
