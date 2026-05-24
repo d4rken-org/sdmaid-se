81145f104c2111967d42f6570f8ea938239ec01d
Matthias Urhahn <darken@darken.eu>
2026-05-24 14:55:48 +0200

    SystemCleaner: Restrict MacFiles filter matches
    Split macOS cleanup matching into file-only metadata and volume-root directory artifacts. AppleDouble sidecars and .DS_Store now require regular files, while macOS volume folders are limited to SDCARD/PORTABLE roots.
    
    This prevents dot-underscore directories such as Pictures/._HLE from being reported as MacFiles cleanup candidates.

--- app-tool-systemcleaner/src/test/java/eu/darken/sdmse/systemcleaner/core/filter/stock/MacFilesFilterTest.kt
+++ app-tool-systemcleaner/src/test/java/eu/darken/sdmse/systemcleaner/core/filter/stock/MacFilesFilterTest.kt
@@ -11,6 +11,11 @@ import org.junit.jupiter.api.Test
 
 class MacFilesFilterTest : SystemCleanerFilterTest() {
 
+    private val volumeRootAreas = setOf(
+        DataArea.Type.SDCARD,
+        DataArea.Type.PORTABLE,
+    )
+
     @BeforeEach
     override fun setup() {
         super.setup()
@@ -29,37 +34,85 @@ class MacFilesFilterTest : SystemCleanerFilterTest() {
         gatewaySwitch = gatewaySwitch,
     )
 
-    @Test fun testFilter() = runTest {
-        mockDefaults()
+    private suspend fun forEachTargetArea(block: suspend (DataArea.Type) -> Unit) {
         val areas = create().targetAreas()
         areaManager.currentAreas()
             .filter { areas.contains(it.type) }
             .distinctBy { it.type }
-            .forEach {
-                val loc = it.type
-                neg(loc, "folder", Flag.Dir)
-                pos(loc, "._something", Flag.File)
-                pos(loc, "folder/._something", Flag.File)
-                pos(loc, "._rollkuchen#,'Ä", Flag.File)
-                pos(loc, "folder/._rollkuchen#,'Ä", Flag.File)
-                pos(loc, ".Trashes", Flag.File)
-                pos(loc, "folder/.Trashes", Flag.File)
-                pos(loc, "._.Trashes", Flag.File)
-                pos(loc, "folder/._.Trashes", Flag.File)
-                pos(loc, ".spotlight", Flag.File)
-                pos(loc, "folder/.spotlight", Flag.File)
-                pos(loc, ".Spotlight-V100", Flag.File)
-                pos(loc, "folder/.Spotlight-V100", Flag.File)
-                pos(loc, ".DS_Store", Flag.File)
-                pos(loc, "folder/.DS_Store", Flag.File)
-                pos(loc, ".fseventsd", Flag.File)
-                pos(loc, "folder/.fseventsd", Flag.File)
-                pos(loc, ".TemporaryItems", Flag.File)
-                pos(loc, "folder/.TemporaryItems", Flag.File)
-            }
+            .forEach { block(it.type) }
+    }
+
+    @Test fun `matches macOS file artifacts only as files`() = runTest {
+        mockDefaults()
+        forEachTargetArea { loc ->
+            neg(loc, "folder", Flag.Dir)
+            if (loc == DataArea.Type.PUBLIC_MEDIA) neg(loc, "Pictures", Flag.Dir)
+
+            pos(loc, "._something", Flag.File)
+            pos(loc, "folder/._something", Flag.File)
+            pos(loc, "Pictures/._HLE", Flag.File)
+            pos(loc, "._rollkuchen#,'Ä", Flag.File)
+            pos(loc, "folder/._rollkuchen#,'Ä", Flag.File)
+            pos(loc, "._.Trashes", Flag.File)
+            pos(loc, "folder/._.Trashes", Flag.File)
+            pos(loc, ".DS_Store", Flag.File)
+            pos(loc, "folder/.DS_Store", Flag.File)
+
+            neg(loc, ".Trashes", Flag.File)
+            neg(loc, "folder/.Trashes", Flag.File)
+            neg(loc, ".spotlight", Flag.File)
+            neg(loc, "folder/.spotlight", Flag.File)
+            neg(loc, ".Spotlight-V100", Flag.File)
+            neg(loc, "folder/.Spotlight-V100", Flag.File)
+            neg(loc, ".fseventsd", Flag.File)
+            neg(loc, "folder/.fseventsd", Flag.File)
+            neg(loc, ".TemporaryItems", Flag.File)
+            neg(loc, "folder/.TemporaryItems", Flag.File)
+        }
         neg(DataArea.Type.PUBLIC_DATA, "._rollkuchen#,'Ä", Flag.File)
         neg(DataArea.Type.PUBLIC_DATA, "some.pkg", Flag.Dir)
         neg(DataArea.Type.PUBLIC_DATA, "some.pkg/._rollkuchen#,'Ä", Flag.File)
+        confirm(create())
+    }
+
+    @Test fun `matches macOS directory artifacts only as directories`() = runTest {
+        mockDefaults()
+        forEachTargetArea { loc ->
+            neg(loc, "folder", Flag.Dir)
+            if (loc == DataArea.Type.PUBLIC_MEDIA) neg(loc, "Pictures", Flag.Dir)
+
+            if (loc in volumeRootAreas) {
+                pos(loc, ".Trashes", Flag.Dir)
+                pos(loc, ".spotlight", Flag.Dir)
+                pos(loc, ".Spotlight-V100", Flag.Dir)
+                pos(loc, ".fseventsd", Flag.Dir)
+                pos(loc, ".TemporaryItems", Flag.Dir)
+            } else {
+                neg(loc, ".Trashes", Flag.Dir)
+                neg(loc, ".spotlight", Flag.Dir)
+                neg(loc, ".Spotlight-V100", Flag.Dir)
+                neg(loc, ".fseventsd", Flag.Dir)
+                neg(loc, ".TemporaryItems", Flag.Dir)
+            }
+
+            neg(loc, "._something", Flag.Dir)
+            neg(loc, "folder/._something", Flag.Dir)
+            neg(loc, "Pictures/._HLE", Flag.Dir)
+            neg(loc, "._rollkuchen#,'Ä", Flag.Dir)
+            neg(loc, "folder/._rollkuchen#,'Ä", Flag.Dir)
+            neg(loc, "._.Trashes", Flag.Dir)
+            neg(loc, "folder/._.Trashes", Flag.Dir)
+            neg(loc, ".DS_Store", Flag.Dir)
+            neg(loc, "folder/.DS_Store", Flag.Dir)
+            neg(loc, "folder/.Trashes", Flag.Dir)
+            neg(loc, "folder/.spotlight", Flag.Dir)
+            neg(loc, "folder/.Spotlight-V100", Flag.Dir)
+            neg(loc, "folder/.fseventsd", Flag.Dir)
+            neg(loc, "folder/.TemporaryItems", Flag.Dir)
+        }
+        neg(DataArea.Type.PUBLIC_DATA, "._rollkuchen#,'Ä", Flag.Dir)
+        neg(DataArea.Type.PUBLIC_DATA, "some.pkg", Flag.Dir)
+        neg(DataArea.Type.PUBLIC_DATA, "some.pkg/._rollkuchen#,'Ä", Flag.Dir)
         confirm(create())
     }
 }
